075a648061230e30950093d6ea0958fbf19c8086
ZhangLiang <qimuyangtao@gmail.com>
2018-06-06 02:23:21 +0800

    z

--- mysql.py
+++ mysql.py
@@ -4,32 +4,34 @@ import pymysql
 from withs import withs
 
 
-class connect:
+class Connect:
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
 
     def __enter__(self):
-        self.connect = pymysql.connect(*self.args, **self.kwargs)
-        return self.connect
+        _connect = pymysql.connect(*self.args, **self.kwargs)
+        self._connect = _connect
+        return _connect
 
     def __exit__(self, type, value, traceback):
-        _connect = self.connect
-        del self.connect
+        _connect = self._connect
+        del self._connect
         _connect.close()
 
 
-class cursor:
+class Cursor:
     def __init__(self, connect):
         self.connect = connect
 
     def __enter__(self):
-        self.cursor = self.connect.cursor(cursor=pymysql.cursors.DictCursor)
-        return self.cursor
+        _cursor = self.connect.cursor(cursor=pymysql.cursors.DictCursor)
+        self._cursor = _cursor
+        return _cursor
 
     def __exit__(self, type, value, traceback):
-        _cursor = self.cursor
-        del self.cursor
+        _cursor = self._cursor
+        del self._cursor
         try:
             if type is None:
                 self.connect.commit()
@@ -37,3 +39,6 @@ class cursor:
                 self.connect.rollback()
         finally:
             _cursor.close()
+
+
+Database = withs(Connect, Cursor)
