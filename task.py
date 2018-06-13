e666de93a65b0a9a4cfc07a6d2698215b40b04c5
ZhangLiang <qimuyangtao@gmail.com>
2018-06-13 15:34:08 +0800

    new task

--- task.py
+++ task.py
@@ -8,53 +8,63 @@ TAG = 'task'
 
 class Task:
     def __init__(self, handle, target):
-        self.handle = handle
-        self.target = target
-        self.result = None
+        self.__handle = handle
+        self.__target = target
+        self.result = False
 
     # 执行任务
+    @synchronized()
     def execute(self):
         try:
-            self.handle(self.target)
+            if not self.result:
+                self.__handle(self.__target)
+                self.result = True
+                del self.__target
+                del self.__handle
         except BaseException as e:
             Log.e(e, tag=TAG)
-            self.result = False
-        else:
-            self.result = True
         return self.result
+
+
+class _Executor:
+    def __init__(self, *tasks):
+        self.tasks = list(tasks)
+        self.backs = 0
+
+    @synchronized()
+    def pop(self):
+        if self.tasks:
+            return self.tasks.pop()
+        return None
+
+    @synchronized()
+    def push(self):
+        self.backs += 1
+
+    # 批量执行任务
+    def execute(self, t):
+        e = self
+
+        class Thread(threading.Thread):
+            def run(self):
+                while True:
+                    task = e.pop()
+                    if task is None:
+                        break
+                    if task.execute():
+                        e.push()
+        threads = [Thread() for i in range(t)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        return self.backs
 
 
 class Tasks:
     def __init__(self, handle, *targets):
         self.tasks = [Task(handle, target) for target in targets]
 
-    def __push(self):
-        self.__tasks = []
-        for task in self.tasks:
-            if not task.result:
-                self.__tasks.append(task)
-
-    @synchronized()
-    def __pop(self):
-        if self.__tasks:
-            return self.__tasks.pop()
-        return None
-
     # 批量执行任务
     def execute(self, t=1):
-        self.__push()
-        pop = self.__pop
-
-        class Thread(threading.Thread):
-            def run(self):
-                while True:
-                    task = pop()
-                    if task is None:
-                        break
-                    task.execute()
-        threads = [Thread() for i in range(t)]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-        return [task.result for task in self.tasks].count(True)
+        return _Executor(*self.tasks).execute(t)
