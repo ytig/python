340234dfb2f3530c1847fc9f9a5c97177f1930e3
ZhangLiang <qimuyangtao@gmail.com>
2018-07-05 12:03:57 +0800

    update decorator

--- decorator.py
+++ decorator.py
@@ -3,10 +3,22 @@
 import inspect
 import threading
 _LOCK = threading.Lock()  # 全局锁
-_CLASSES = {}  # 伪类
+_MODULES = {}  # 伪模块
+_CLASSES = {}  # 伪类型
 
 
-# 获取伪类
+# 获取伪模块
+def moduleOf(generics):
+    if isinstance(generics, str):
+        with _LOCK:
+            if generics not in _MODULES:
+                _MODULES[generics] = lambda: generics
+            return _MODULES[generics]
+    else:
+        return moduleOf(inspect.getmodule(generics).__name__)
+
+
+# 获取伪类型
 def classOf(generics):
     if isinstance(generics, str):
         with _LOCK:
@@ -26,132 +38,112 @@ def classOf(generics):
 class Lock:
     def __init__(self, object):
         self.object = object
-        self.name = '__LOCK__' if inspect.isclass(object) else '__lock__'
+
+    @staticmethod
+    def __lockOf(object):
+        if inspect.ismodule(object):
+            name = '__LOCK__'
+        elif inspect.isclass(object):
+            name = '__Lock__'
+        else:
+            name = '__lock__'
+        if not hasattr(object, name):
+            setattr(object, name, (threading.Lock(), [],))
+        return getattr(object, name)
 
     def __enter__(self):
-        tn = threading.current_thread().name
-        with _LOCK:
-            if not hasattr(self.object, self.name):
-                setattr(self.object, self.name, (threading.Lock(), [],))
-            lock, stack = getattr(self.object, self.name)
-            a = tn not in stack
-            if not a:
-                stack.append(tn)
-        if a:
-            lock.acquire()
+        if self.object is not None:
+            tn = threading.current_thread().name
             with _LOCK:
-                stack.append(tn)
+                lock, stack = Lock.__lockOf(self.object)
+                a = tn not in stack
+                if not a:
+                    stack.append(tn)
+            if a:
+                lock.acquire()
+                with _LOCK:
+                    stack.append(tn)
 
     def __exit__(self, type, value, traceback):
-        with _LOCK:
-            lock, stack = getattr(self.object, self.name)
-            stack.pop()
-            r = len(stack) == 0
-        if r:
-            lock.release()
+        if self.object is not None:
+            with _LOCK:
+                lock, stack = Lock.__lockOf(self.object)
+                stack.pop()
+                r = len(stack) == 0
+            if r:
+                lock.release()
 
 
-LOCK_CLASS = 0b1  # 类锁
-LOCK_INSTANCE = 0b10  # 实例锁
+LOCK_INSTANCE = 0b1  # 实例锁
+LOCK_CLASS = 0b10  # 类型锁
+LOCK_MODULE = 0b100  # 模块锁
 
 
 # 加锁同步函数
 def synchronized(lock=LOCK_INSTANCE):
-    lc = True if lock & LOCK_CLASS else False
-    li = True if lock & LOCK_INSTANCE else False
+    i = True if lock & LOCK_INSTANCE else False
+    c = True if lock & LOCK_CLASS else False
+    m = True if lock & LOCK_MODULE else False
 
     def decorator(function):
-        cls = classOf(function)
-        if li:
-            if lc:
-                def wrapper(self, *args, **kwargs):
-                    with Lock(cls):
-                        with Lock(self):
-                            return function(self, *args, **kwargs)
-            else:
-                def wrapper(self, *args, **kwargs):
-                    with Lock(self):
-                        return function(self, *args, **kwargs)
-        else:
-            if lc:
-                def wrapper(*args, **kwargs):
-                    with Lock(cls):
+        lm = Lock(moduleOf(function) if m else None)
+        lc = Lock(classOf(function) if c else None)
+
+        def wrapper(*args, **kwargs):
+            if i and len(args) <= 0:
+                raise Exception('no self.')
+            li = Lock(args[0] if i else None)
+            with lm:
+                with lc:
+                    with li:
                         return function(*args, **kwargs)
-            else:
-                def wrapper(*args, **kwargs):
-                    return function(*args, **kwargs)
         return wrapper
     return decorator
 
 
 # 单次调用函数
 def disposable(static=False, repeat=None):
-    if isinstance(repeat, str):
-        def decorator(function):
-            name = function.__qualname__
-            if not static:
-                def wrapper(self, *args, **kwargs):
+    if not isinstance(repeat, str) and not callable(repeat):
+        repeat = lambda *args, **kwargs: None
 
-                    if not getattr(self, name, False):
-                        kwargs.update({repeat: False, })
-                        try:
-                            r = function(self, *args, **kwargs)
-                        except BaseException as e:
-                            raise e
-                        else:
-                            setattr(self, name, True)
-                            return r
-                    else:
-                        kwargs.update({repeat: True, })
-                        return function(self, *args, **kwargs)
+    def call(function, object, name, *args, **kwargs):
+        if isinstance(repeat, str):
+            if not getattr(object, name, False):
+                kwargs.update({repeat: False, })
+                try:
+                    r = function(*args, **kwargs)
+                except BaseException as e:
+                    raise e
+                else:
+                    setattr(object, name, True)
+                    return r
             else:
-                cls = classOf(function)
-
-                def wrapper(*args, **kwargs):
-                    if not getattr(cls, name, False):
-                        kwargs.update({repeat: False, })
-                        try:
-                            r = function(*args, **kwargs)
-                        except BaseException as e:
-                            raise e
-                        else:
-                            setattr(cls, name, True)
-                            return r
-                    else:
-                        kwargs.update({repeat: True, })
-                        return function(*args, **kwargs)
-            return wrapper
-    else:
-        if not callable(repeat):
-            repeat = lambda *args, **kwargs: None
-
-        def decorator(function):
-            name = function.__qualname__
-            if not static:
-                def wrapper(self, *args, **kwargs):
-                    if not getattr(self, name, False):
-                        try:
-                            r = function(self, *args, **kwargs)
-                        except BaseException as e:
-                            raise e
-                        else:
-                            setattr(self, name, True)
-                            return r
-                    else:
-                        return repeat(self, *args, **kwargs)
+                kwargs.update({repeat: True, })
+                return function(*args, **kwargs)
+        else:
+            if not getattr(object, name, False):
+                try:
+                    r = function(*args, **kwargs)
+                except BaseException as e:
+                    raise e
+                else:
+                    setattr(object, name, True)
+                return r
             else:
-                cls = classOf(function)
+                return repeat(*args, **kwargs)
 
-                def wrapper(*args, **kwargs):
-                    if not getattr(cls, name, False):
-                        try:
-                            r = function(*args, **kwargs)
-                        except BaseException as e:
-                            raise e
-                        else:
-                            setattr(cls, name, True)
-                            return r
-                    else:
-                        return repeat(*args, **kwargs)
-            return wrapper
+    def decorator(function):
+        name = function.__qualname__
+        if static:
+            module = moduleOf(function)
+
+        def wrapper(*args, **kwargs):
+            if static:
+                object = module
+            else:
+                if len(args) <= 0:
+                    raise Exception('no self.')
+                object = args[0]
+            return call(function, object, name, *args, **kwargs)
+        return wrapper
     return decorator
