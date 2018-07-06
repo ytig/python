56a47ae9fd22fe7fb557a23b41bd82bf73b51661
ZhangLiang <qimuyangtao@gmail.com>
2018-07-06 11:40:29 +0800

    z

--- task.py
+++ task.py
@@ -6,7 +6,7 @@ TAG = __name__
 
 
 class Task:
-    def __init__(self, handle, target, log=lambda e: __import__('log').Log.e(e, tag=TAG)):
+    def __init__(self, handle, target, log=lambda e: log.Log.e(e, tag=TAG)):
         self.__handle = handle
         self.__target = target
         self.__log = log
@@ -30,49 +30,47 @@ class Task:
         return self.result
 
 
-class _Executor:
-    def __init__(self, *tasks):
-        self.tasks = list(tasks)
-        self.backs = 0
-
-    @synchronized()
-    def pop(self):
-        if self.tasks:
-            return self.tasks.pop()
-        return None
-
-    @synchronized()
-    def push(self):
-        self.backs += 1
-
-    # 批量执行任务
-    @throwaway()
-    def execute(self, t):
-        e = self
-
-        class Thread(threading.Thread):
-            def run(self):
-                while True:
-                    task = e.pop()
-                    if task is None:
-                        break
-                    if task.execute():
-                        e.push()
-        threads = [Thread() for i in range(t)]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-        return self.backs
-
-
 class Tasks:
+    class Executor:
+        def __init__(self, *tasks):
+            self.tasks = list(tasks)
+            self.backs = 0
+
+        @synchronized()
+        def pop(self):
+            if self.tasks:
+                return self.tasks.pop()
+            return None
+
+        @synchronized()
+        def push(self):
+            self.backs += 1
+
+        @throwaway()
+        def execute(self, t):
+            e = self
+
+            class Thread(threading.Thread):
+                def run(self):
+                    while True:
+                        task = e.pop()
+                        if task is None:
+                            break
+                        if task.execute():
+                            e.push()
+            threads = [Thread() for i in range(t)]
+            for thread in threads:
+                thread.start()
+            for thread in threads:
+                thread.join()
+            return self.backs
+
     def __init__(self, handle, *targets, **kwargs):
         self.tasks = [Task(handle, target, **kwargs) for target in targets]
 
     # 批量执行任务
     def execute(self, t=1):
-        return _Executor(*self.tasks).execute(t)
+        return Tasks.Executor(*self.tasks).execute(t)
 
 
 class Queue:
@@ -82,38 +80,44 @@ class Queue:
             self.queues = []
             self.running = 0
 
+    class Executor(threading.Thread):
+        def __init__(self, mutex):
+            super().__init__()
+            self.mutex = mutex
+
+        def run(self):
+            while True:
+                with Lock(self.mutex):
+                    if self.mutex.queues:
+                        queue = self.mutex.queues.pop(0)
+                    else:
+                        self.mutex.running -= 1
+                        break
+                try:
+                    queue.pop()
+                except BaseException as e:
+                    try:
+                        getattr(queue.__class__, 'log', None)(e)
+                    except BaseException:
+                        pass
+
     # 打印日志
     @staticmethod
     def log(e):
-        __import__('log').Log.e(e, tag=TAG)
+        log.Log.e(e, tag=TAG)
 
     # 分派任务
     def push(self):
-        log = getattr(self.__class__, 'log', None)
         mutex = Queue.Mutex.instanceOf(classOf(self)())
         with Lock(mutex):
             mutex.queues.append(self)
             if mutex.running < 1:
                 mutex.running += 1
-
-                class Thread(threading.Thread):
-                    def run(self):
-                        while True:
-                            with Lock(mutex):
-                                if mutex.queues:
-                                    queue = mutex.queues.pop(0)
-                                else:
-                                    mutex.running -= 1
-                                    break
-                            try:
-                                queue.pop()
-                            except BaseException as e:
-                                try:
-                                    log(e)
-                                except BaseException:
-                                    pass
-                Thread().start()
+                Queue.Executor(mutex).start()
 
     # 执行任务
     def pop(self):
         pass
+
+
+import log
