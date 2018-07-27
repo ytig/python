7c76e622ba16a734839705e326d2751f15ad487f
ZhangLiang <qimuyangtao@gmail.com>
2018-07-27 14:49:30 +0800

    !pickle

--- session.py
+++ session.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
-import pickle
+import json
 import requests
+from http.cookiejar import Cookie
 from decorator import Lock, synchronized, throwaway
 
 
@@ -8,7 +9,32 @@ from decorator import Lock, synchronized, throwaway
 def cookies2string(cookies):
     string = ''
     if cookies:
-        string = pickle.dumps(cookies._cookies).hex()
+        _cookies = {}
+        for domain, paths, in cookies._cookies.items():
+            _cookies[domain] = {}
+            for path, names, in paths.items():
+                _cookies[domain][path] = {}
+                for name, cookie, in names.items():
+                    _cookies[domain][path][name] = {
+                        'version': cookie.version,
+                        'name': cookie.name,
+                        'value': cookie.value,
+                        'port': cookie.port,
+                        'port_specified': cookie.port_specified,
+                        'domain': cookie.domain,
+                        'domain_specified': cookie.domain_specified,
+                        'domain_initial_dot': cookie.domain_initial_dot,
+                        'path': cookie.path,
+                        'path_specified': cookie.path_specified,
+                        'secure': cookie.secure,
+                        'expires': cookie.expires,
+                        'discard': cookie.discard,
+                        'comment': cookie.comment,
+                        'comment_url': cookie.comment_url,
+                        'rest': cookie._rest,
+                        'rfc2109': cookie.rfc2109,
+                    }
+        string = json.dumps(_cookies)
     return string
 
 
@@ -16,7 +42,16 @@ def cookies2string(cookies):
 def string2cookies(string):
     cookies = requests.cookies.cookiejar_from_dict({})
     if string:
-        cookies._cookies = pickle.loads(bytes.fromhex(string))
+        try:
+            _cookies = json.loads(string)
+            for domain, paths, in _cookies.items():
+                cookies._cookies[domain] = {}
+                for path, names, in paths.items():
+                    cookies._cookies[domain][path] = {}
+                    for name, cookie, in names.items():
+                        cookies._cookies[domain][path][name] = Cookie(**cookie)
+        except BaseException:
+            pass
     return cookies
 
 
