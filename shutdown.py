8a94588c79f13fa5eaefe12dd72afb54950f997e
ZhangLiang <qimuyangtao@gmail.com>
2018-07-29 17:19:02 +0800

    ab register

--- shutdown.py
+++ shutdown.py
@@ -1,55 +1,81 @@
 #!/usr/local/bin/python3
 import threading
-from decorator import LOCK_MODULE, synchronized, throwaway
-_events = []  # 事件
+_LOCK = threading.Lock()  # 全局锁
+_EVENT_TYPE_BEFORE_SHUTDOWN = 'b'  # 关闭前
+_EVENT_TYPE_AFTER_SHUTDOWN = 'a'  # 关闭后
+if threading.current_thread() is not threading.main_thread():
+    raise Exception('cannot import at thread.')
+if threading.main_thread().is_alive():
+    _events = []
+
+    def decorator(function):
+        def wrapper():
+            with _LOCK:
+                global _events
+                events = _events.copy()
+                _events = None
+            for type, func, args, kwargs, in events:
+                if type == _EVENT_TYPE_BEFORE_SHUTDOWN:
+                    try:
+                        func(*args, **kwargs)
+                    except BaseException:
+                        pass
+            ret = function()
+            for type, func, args, kwargs, in events:
+                if type == _EVENT_TYPE_AFTER_SHUTDOWN:
+                    try:
+                        func(*args, **kwargs)
+                    except BaseException:
+                        pass
+            return ret
+        return wrapper
+    threading._shutdown = decorator(threading._shutdown)
+    del decorator
+else:
+    _events = None
 
 
-# 注册
-@synchronized(lock=LOCK_MODULE)
-def register(*args, **kwargs):
+# 注册（关闭前）
+def bregister(*args, **kwargs):
     if len(args) <= 0:
         raise Exception('no func.')
     func = args[0]
     args = args[1:]
-    if isinstance(_events, list):
-        _events.append((func, args, kwargs,))
-        _hook()
-        return True
-    else:
-        return False
+    with _LOCK:
+        if isinstance(_events, list):
+            _events.append((_EVENT_TYPE_BEFORE_SHUTDOWN, func, args, kwargs,))
+            return
+    try:
+        func(*args, **kwargs)
+    except BaseException:
+        pass
+
+
+# 注册（关闭后）
+def aregister(*args, **kwargs):
+    if len(args) <= 0:
+        raise Exception('no func.')
+    func = args[0]
+    args = args[1:]
+    with _LOCK:
+        if isinstance(_events, list):
+            _events.append((_EVENT_TYPE_AFTER_SHUTDOWN, func, args, kwargs,))
+            return
+    try:
+        func(*args, **kwargs)
+    except BaseException:
+        pass
 
 
 # 注销
-@synchronized(lock=LOCK_MODULE)
 def unregister(func):
-    r = 0
-    if isinstance(_events, list):
-        i = len(_events) - 1
-        while i >= 0:
-            if _events[i][0] is func:
-                _events.pop(i)
-                r += 1
-            i -= 1
-    return r
-
-
-@synchronized(lock=LOCK_MODULE)
-def _shutdown():
-    global _events
-    while _events:
-        func, args, kwargs = _events.pop()
-        func(*args, **kwargs)
-    _events = None
-
-
-@throwaway(static=True)
-def _hook():
-    if threading.main_thread().is_alive():
-        def decorator(function):
-            def wrapper():
-                _shutdown()
-                return function()
-            return wrapper
-        threading._shutdown = decorator(threading._shutdown)
-    else:
-        _shutdown()
+    with _LOCK:
+        r = 0
+        if isinstance(_events, list):
+            i = len(_events) - 1
+            while i >= 0:
+                if _events[i][1] is func:
+                    _events.pop(i)
+                    r += 1
+                i -= 1
+        return r
