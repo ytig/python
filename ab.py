4965085d6d2efb9b16a1efacf8064238a244eb1e
ZhangLiang <qimuyangtao@gmail.com>
2018-08-03 12:39:51 +0800

    z

--- ab.py
+++ ab.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import weakref
 import inspect
+from kit import search, hasvar, getvar, setvar
 from decorator import ilock, ithrow
 from shutdown import bregister, aregister, unregister
 
@@ -17,92 +18,92 @@ class weakmethod:
         if obj is not None:
             return getattr(obj, self.name)(*args, **kwargs)
         else:
-            return __class__.GC
+            return weakmethod.GC
 
 
-class attribute:
+class build:
     def __init__(self, bases, namespace):
-        self.bases = bases
+        self.bases = tuple(search(lambda cls: cls.__bases__).depth(*bases))
         self.namespace = namespace
 
-    # 检查属性
-    def hasattr(self, name):
-        if name in self.namespace:
+    # 检查变量
+    def hasvar(self, k):
+        if k in self.namespace:
             return True
-        for base in self.bases:
-            if hasattr(base, name):
+        for b in self.bases:
+            if hasvar(b, k):
                 return True
         return False
 
-    # 获取属性
-    def getattr(self, name, default=None):
-        if name in self.namespace:
-            return self.namespace.get(name)
-        for base in self.bases:
-            if hasattr(base, name):
-                return getattr(base, name)
-        return default
+    # 获取变量
+    def getvar(self, k, d=None):
+        if k in self.namespace:
+            return self.namespace.get(k)
+        for b in self.bases:
+            if hasvar(b, k):
+                return getvar(b, k)
+        return d
 
-    # 设置属性
-    def setattr(self, name, value):
-        attr = self.getattr(name)
-        if isinstance(value, staticmethod):
-            new = value.__func__
-            old = attr.__func__ if isinstance(attr, staticmethod) else None
-            self.namespace[name] = staticmethod(lambda *args, **kwargs: new(old, *args, **kwargs))
-        elif isinstance(value, classmethod):
-            new = value.__func__
-            old = attr.__func__ if isinstance(attr, classmethod) else None
-            self.namespace[name] = classmethod(lambda cls, *args, **kwargs: new(cls, old, *args, **kwargs))
-        elif isinstance(value, property):
-            nget, nset, ndel, = (value.fget, value.fset, value.fdel,)
-            oget, oset, odel, = (attr.fget, attr.fset, attr.fdel,) if isinstance(attr, property) else (None, None, None,)
-            self.namespace[name] = property(fget=lambda self: nget(self, oget) if nget else None, fset=lambda self, value: nset(self, oset, value) if nset else None, fdel=lambda self: ndel(self, odel) if ndel else None)
-        elif inspect.isfunction(value):
-            new = value
-            old = attr if inspect.isfunction(attr) else None
-            self.namespace[name] = lambda self, *args, **kwargs: new(self, old, *args, **kwargs)
+    # 设置变量
+    def setvar(self, k, v):
+        var = self.getvar(k)
+        if isinstance(v, staticmethod):
+            new = v.__func__
+            old = var.__func__ if isinstance(var, staticmethod) else None
+            self.namespace[k] = staticmethod(lambda *args, **kwargs: new(old, *args, **kwargs))
+        elif isinstance(v, classmethod):
+            new = v.__func__
+            old = var.__func__ if isinstance(var, classmethod) else None
+            self.namespace[k] = classmethod(lambda cls, *args, **kwargs: new(cls, old, *args, **kwargs))
+        elif isinstance(v, property):
+            nget, nset, ndel, = (v.fget, v.fset, v.fdel,)
+            oget, oset, odel, = (var.fget, var.fset, var.fdel,) if isinstance(var, property) else (None, None, None,)
+            self.namespace[k] = property(fget=lambda self: nget(self, oget) if nget else None, fset=lambda self, value: nset(self, oset, value) if nset else None, fdel=lambda self: ndel(self, odel) if ndel else None)
+        elif inspect.isfunction(v):
+            new = v
+            old = var if inspect.isfunction(var) else None
+            self.namespace[k] = lambda self, *args, **kwargs: new(self, old, *args, **kwargs)
         else:
-            self.namespace[name] = value
+            self.namespace[k] = v
 
 
 class ABMeta(type):
     def __new__(mcls, name, bases, namespace, **kwargs):
-        attr = attribute(bases, namespace)
+        builder = build(bases, namespace)
 
         def __init__(self, *args, **kwargs):
-            setattr(self, '__weakbef__', weakmethod(self, '__bef__'))
-            setattr(self, '__weakaft__', weakmethod(self, '__aft__'))
+            setvar(self, '__weakbef__', weakmethod(self, '__bef__'))
+            setvar(self, '__weakaft__', weakmethod(self, '__aft__'))
             func = args[0]
             args = args[1:]
             if callable(func):
                 func(self, *args, **kwargs)
-            bregister(getattr(self, '__weakbef__'))
-            aregister(getattr(self, '__weakaft__'))
-        attr.setattr('__init__', __init__)
+            bregister(getvar(self, '__weakbef__'))
+            aregister(getvar(self, '__weakaft__'))
+        builder.setvar('__init__', __init__)
 
         def __bef__(self, *args, **kwargs):
             func = args[0]
             args = args[1:]
             if callable(func):
                 func(self, *args, **kwargs)
-        attr.setattr('__bef__', __bef__)
+        builder.setvar('__bef__', __bef__)
 
         def __aft__(self, *args, **kwargs):
             func = args[0]
             args = args[1:]
             if callable(func):
                 func(self, *args, **kwargs)
-        attr.setattr('__aft__', __aft__)
+        builder.setvar('__aft__', __aft__)
 
         @ilock()
         @ithrow()
         def __del__(self, *args, **kwargs):
-            unregister(getattr(self, '__weakaft__'))
-            unregister(getattr(self, '__weakbef__'))
+            unregister(getvar(self, '__weakaft__'))
+            unregister(getvar(self, '__weakbef__'))
             func = args[0]
             args = args[1:]
             if callable(func):
                 func(self, *args, **kwargs)
-        attr.setattr('__del__', __del__)
+        builder.setvar('__del__', __del__)
         return super().__new__(mcls, name, bases, namespace, **kwargs)
