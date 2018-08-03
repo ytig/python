3f85f7a5164395b90f0605fe561d27b8695545e0
ZhangLiang <qimuyangtao@gmail.com>
2018-08-03 23:12:48 +0800

    invoke

--- ab.py
+++ ab.py
@@ -6,90 +6,76 @@ from decorator import ilock, ithrow
 from shutdown import bregister, aregister, unregister
 
 
-# 定义类
-def define(super, ignore=['__class__']):
-    def parse(f_locals):
-        cls = f_locals['cls']
-        args = f_locals['args']
-        kwargs = f_locals['kwargs']
-        __dict__ = dict((k, v,) for k, v, in f_locals.items() if k not in ['cls', 'args', 'kwargs', ] + ignore)
-        return cls, args, kwargs, __dict__,
-    cls, args, kwargs, __dict__, = parse(inspect.currentframe().f_back.f_locals)
-    bases = tuple(search(lambda cls: cls.__bases__).depth(*args[1]))
-    namespace = args[2]
-
-    def _getvar(key, default=None):
+# 定义类型
+def define(super, ignore=('__class__',)):
+    CLS = lambda *args, **kwargs: args[0] is __class__
+    SELF = lambda *args, **kwargs: args[0].__class__ is __class__
+    args, kwargs, __dict__, = _roll(inspect.currentframe().f_back.f_locals, ignore)
+    bases = tuple(search(lambda cls: cls.__bases__).depth(*args[2]))
+    namespace = args[3]
+    for key, var, in __dict__.items():
+        _var = None
         if key in namespace:
-            return namespace.get(key)
+            _var = namespace.get(key)
         else:
             for b in bases:
                 if hasvar(b, key):
-                    return getvar(b, key)
-        return default
-
-    def _setvar(key, var):
-        _var = _getvar(key)
+                    _var = getvar(b, key)
+                    break
         if isinstance(var, staticmethod):
-            var = var.__func__
-            _var = _var.__func__ if isinstance(_var, staticmethod) else None
-
-            def __var__(*args, **kwargs):
-                # todo
-                pass
-            namespace[key] = staticmethod(__var__)
+            # todo
+            pass
         elif isinstance(var, classmethod):
             var = var.__func__
             _var = _var.__func__ if isinstance(_var, classmethod) else None
-
-            def __var__(cls, *args, **kwargs):
-                if cls is __class__:
-                    return var(cls, _var, *args, **kwargs)
-                elif _var is not None:
-                    return _var(cls, *args, **kwargs)
-            namespace[key] = classmethod(__var__)
+            namespace[key] = classmethod(_fork(CLS, var, _var))
         elif isinstance(var, property):
             fget, fset, fdel, = (var.fget, var.fset, var.fdel,)
             _fget, _fset, _fdel, = (_var.fget, _var.fset, _var.fdel,) if isinstance(_var, property) else (None, None, None,)
-            if fget:
-                def __fget__(self):
-                    if self.__class__ is __class__:
-                        return fget(self, _fget)
-                    elif _fget is not None:
-                        return _fget(self)
-            else:
-                __fget__ = None
-            if fset:
-                def __fset__(self, value):
-                    if self.__class__ is __class__:
-                        return fset(self, _fset, value)
-                    elif _fset is not None:
-                        return _fset(self, value)
-            else:
-                __fset__ = None
-            if fdel:
-                def __fdel__(self):
-                    if self.__class__ is __class__:
-                        return fdel(self, _fdel)
-                    elif _fdel is not None:
-                        return _fdel(self)
-            else:
-                __fdel__ = None
-            namespace[key] = property(fget=__fget__, fset=__fset__, fdel=__fdel__)
+            namespace[key] = property(fget=_fork(SELF, fget, _fget) if fget else None, fset=_fork(SELF, fset, _fset) if fset else None, fdel=_fork(SELF, fdel, _fdel) if fdel else None)
         elif inspect.isfunction(var):
             _var = _var if inspect.isfunction(_var) else None
-
-            def __var__(self, *args, **kwargs):
-                if self.__class__ is __class__:
-                    return var(self, _var, *args, **kwargs)
-                elif _var is not None:
-                    return _var(self, *args, **kwargs)
-            namespace[key] = __var__
+            namespace[key] = _fork(SELF, var, _var)
         else:
             namespace[key] = var
-    for k, v, in __dict__.items():
-        _setvar(k, v)
-    __class__ = super.__new__(cls, *args, **kwargs)
+    __class__ = super.__new__(*args, **kwargs)
     return __class__
+
+
+# 原始调用
+def invoke():
+    f = inspect.currentframe()
+    while f:
+        if f.f_code is _f_code:
+            if f.f_locals['old'] is not None:
+                return f.f_locals['old'](*f.f_locals['args'], **f.f_locals['kwargs'])
+        f = f.f_back
+    return None
+
+
+def _roll(f_locals, f_ignore):
+    args = (f_locals['cls'], ) + f_locals['args']
+    kwargs = f_locals['kwargs']
+    __dict__ = {}
+    for k in f_locals.keys():
+        if k in ['cls', 'args', 'kwargs', ]:
+            continue
+        if k in (f_ignore or []):
+            continue
+        __dict__[k] = f_locals[k]
+    return args, kwargs, __dict__,
+
+
+def _fork(opt, new, old):
+    def wrapper(*args, **kwargs):
+        if opt(*args, **kwargs):
+            return new(*args, **kwargs)
+        elif old is not None:
+            return old(*args, **kwargs)
+    return wrapper
+
+
+_f_code = _fork(None, None, None).__code__
 
 
 class weakmethod:
@@ -109,39 +95,24 @@ class weakmethod:
 
 class ABMeta(type):
     def __new__(cls, *args, **kwargs):
-        def __init__(*args, **kwargs):
-            ret = None
-            self = args[0]
+        def __init__(self, *args, **kwargs):
             setvar(self, '__weakbef__', weakmethod(self, '__bef__'))
             setvar(self, '__weakaft__', weakmethod(self, '__aft__'))
-            if args[1] is not None:
-                ret = args[1](self, *args[2:], **kwargs)
+            ret = invoke()
             bregister(getvar(self, '__weakbef__'))
             aregister(getvar(self, '__weakaft__'))
             return ret
 
-        def __bef__(*args, **kwargs):
-            ret = None
-            self = args[0]
-            if args[1] is not None:
-                ret = args[1](self, *args[2:], **kwargs)
-            return ret
+        def __bef__(self):
+            return invoke()
 
-        def __aft__(*args, **kwargs):
-            ret = None
-            self = args[0]
-            if args[1] is not None:
-                ret = args[1](self, *args[2:], **kwargs)
-            return ret
+        def __aft__(self):
+            return invoke()
 
         @ilock()
         @ithrow()
-        def __del__(*args, **kwargs):
-            ret = None
-            self = args[0]
+        def __del__(self):
             unregister(getvar(self, '__weakaft__'))
             unregister(getvar(self, '__weakbef__'))
-            if args[1] is not None:
-                ret = args[1](self, *args[2:], **kwargs)
-            return ret
+            return invoke()
         return define(super())
