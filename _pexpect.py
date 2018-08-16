bcafe85dde00a9e048b1306507a2dda49a58e973
ZhangLiang <qimuyangtao@gmail.com>
2018-08-16 18:10:42 +0800

    pexpect

--- _pexpect.py
+++ _pexpect.py
@@ -1,21 +1,21 @@
 #!/usr/local/bin/python3
-from pexpect import *
-import sys as _sys
-_spawn = spawn
+import sys
+import inspect
+import pexpect
 
 
-def spawn(*args, **kwargs):
-    if 'logfile' not in kwargs:
-        logfile = _sys.stdout.buffer
-    else:
-        logfile = kwargs['logfile']
-        del kwargs['logfile']
-    self = _spawn(*args, **kwargs)
-    self.logfile_read = logfile
-    _interact = self.interact
+class spawn(pexpect.spawn):
+    def __init__(self, *args, **kwargs):
+        init = pexpect.spawn.__init__
+        ba = inspect.signature(init).bind(self, *args, **kwargs)
+        if 'logfile' not in ba.arguments:
+            logfile_read = sys.stdout.buffer
+        else:
+            logfile_read = ba.arguments['logfile']
+            ba.arguments['logfile'] = None
+        init(*ba.args, **ba.kwargs)
+        self.logfile_read = logfile_read
 
-    def interact(*args, **kwargs):
+    def interact(self, *args, **kwargs):
         self.logfile_read = None
-        return _interact(*args, **kwargs)
-    self.interact = interact
-    return self
+        return super().interact(*args, **kwargs)
