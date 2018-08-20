64b8c8869b50acbc81564da355f31747d225d3cd
ZhangLiang <qimuyangtao@gmail.com>
2018-08-21 01:03:05 +0800

    new wa

--- _pymysql.py
+++ _pymysql.py
@@ -1,37 +1,29 @@
 #!/usr/local/bin/python3
 import pymysql
-from wa import extends
+from wa import withas
 
 
-@extends
-class Connect:
-    def __init__(self, *args, **kwargs):
-        self.connect = pymysql.connect(*args, **kwargs)
-
-    def __enter__(self):
-        return self.connect
-
-    def __exit__(self, t, v, tb):
-        self.connect.close()
+@withas
+def Connect(*args, **kwargs):
+    connect = pymysql.connect(*args, **kwargs)
+    try:
+        yield connect
+    finally:
+        connect.close()
 
 
-@extends
-class Cursor:
-    def __init__(self, connect):
-        self.connect = connect
-        self.cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)
-
-    def __enter__(self):
-        return self.cursor
-
-    def __exit__(self, t, v, tb):
-        try:
-            if t is None:
-                self.connect.commit()
-            else:
-                self.connect.rollback()
-        finally:
-            self.cursor.close()
+@withas
+def Cursor(connect):
+    cursor = connect.cursor(cursor=pymysql.cursors.DictCursor)
+    try:
+        yield cursor
+    except BaseException:
+        connect.rollback()
+        raise
+    else:
+        connect.commit()
+    finally:
+        cursor.close()
 
 
 Database = Connect.series(Cursor)
--- wa.py
+++ wa.py
@@ -1,6 +1,265 @@
 #!/usr/local/bin/python3
+import inspect
 import threading
+import contextlib
+import collections
 from decorator import ilock
+from task import Tree
+
+
+# 上下文管理器（装饰器）
+def withas(generics):
+    if inspect.isclass(generics):
+        if issubclass(generics, _baseclass):
+            return generics
+        else:
+            class _class(_baseclass):
+                def __init__(self, *args, **kwargs):
+                    self.args = args
+                    self.kwargs = kwargs
+                    self.__obj = Stack()
+
+                def __enter__(self):
+                    obj = generics(*self.args, **self.kwargs)
+                    o = obj.__enter__()
+                    self.__obj.push(obj)
+                    return o
+
+                def __exit__(self, t, v, tb):
+                    obj = self.__obj.pop()
+                    return obj.__exit__(t, v, tb)
+            return _class
+    elif inspect.isfunction(generics):
+        if inspect.isgeneratorfunction(generics):
+            generics = contextlib.contextmanager(generics)
+
+            class _generatorfunction(_baseclass):
+                def __init__(self, *args, **kwargs):
+                    self.args = args
+                    self.kwargs = kwargs
+                    self.__obj = Stack()
+
+                def __enter__(self):
+                    obj = generics(*self.args, **self.kwargs)
+                    o = obj.__enter__()
+                    self.__obj.push(obj)
+                    return o
+
+                def __exit__(self, t, v, tb):
+                    obj = self.__obj.pop()
+                    return obj.__exit__(t, v, tb)
+            return _generatorfunction
+        else:
+            class _function(_baseclass):
+                def __init__(self, *args, **kwargs):
+                    self.args = args
+                    self.kwargs = kwargs
+
+                def __enter__(self):
+                    return generics(*self.args, **self.kwargs)
+
+                def __exit__(self, t, v, tb):
+                    pass
+            return _function
+
+
+# 执行器
+def tree(t, log=None):
+    if t <= 0:
+        def _tree(cpu, *mems):
+            ret = []
+            for mem in mems:
+                try:
+                    ret.append(cpu(mem))
+                except BaseException as e:
+                    try:
+                        log(e)
+                    except BaseException:
+                        pass
+            return ret
+        return _tree
+    else:
+        def _tree(cpu, *mems):
+            return Tree(cpu, *mems, log=log).plant(t=t)
+        return _tree
+
+
+class _baseclass:
+    # 输入转换
+    @classmethod
+    def input(this, pipe):
+        """
+        pipe: *args, **kwargs -> Arguments
+        """
+        series = _baseclass.__dict__['series'].__func__
+        return series(lambda *args, **kwargs: (args, kwargs,), this, pipe=lambda t: pipe(*t[0], **t[1]))
+
+    # 输出转换
+    @classmethod
+    def output(this, pipe):
+        """
+        pipe: object -> object
+        """
+        series = _baseclass.__dict__['series'].__func__
+        return series(this, pipe)
+
+    # 串联流程
+    @classmethod
+    def series(parent, child, pipe=lambda o: Arguments(o)):
+        """
+        pipe: object -> Arguments
+        """
+        parent = withas(parent)
+        child = withas(child)
+
+        def _series(*args, **kwargs):
+            y = False
+            pstack = (lambda i: [i, i.__enter__(), ])(parent(*args, **kwargs))
+            try:
+                cstack = (lambda i: [i, i.__enter__(), ])(pipe(pstack.pop())(child))
+                try:
+                    y = True
+                    yield cstack.pop()
+                except BaseException as e:
+                    if cstack.pop().__exit__(type(e), e, e.__traceback__) is not True:
+                        raise
+                else:
+                    cstack.pop().__exit__(None, None, None)
+            except BaseException as e:
+                if pstack.pop().__exit__(type(e), e, e.__traceback__) is not True:
+                    raise
+                if not y:
+                    yield None
+            else:
+                pstack.pop().__exit__(None, None, None)
+        return withas(_series)
+
+    # 并联流程
+    @classmethod
+    def parallel(husband, *wives, pipe=lambda *args, **kwargs: [Arguments(*args, **kwargs), ], core=tree(0)):
+        """
+        pipe: *args, **kwargs -> Arguments[]
+        """
+        humans = [withas(i) for i in (husband,) + wives]
+
+        def enter(args):
+            call = args[0]
+            arguments = args[1]
+            obj = arguments(call)
+            return [obj, obj.__enter__(), ]
+
+        def exit(args):
+            obj = args[0]
+            e = args[1]
+            if e is None:
+                return obj.__exit__(None, None, None)
+            else:
+                return obj.__exit__(type(e), e, e.__traceback__)
+
+        def _parallel(*args, **kwargs):
+            p = [list(pipe(*args, **kwargs)), ]
+            p[0] += [Arguments() for i in range(len(humans) - len(p[0]))]
+            stacks = core(enter, *[t for t in zip(humans, p.pop())])
+            try:
+                assert len(stacks) == len(humans), 'parallel enter error'
+            except BaseException:
+                for s in stacks:
+                    s.pop()
+                assert len(core(exit, *[(s.pop(), None,) for s in stacks])) == len(stacks), 'parallel exit error'
+                raise
+            else:
+                try:
+                    yield [s.pop() for s in stacks]
+                except BaseException as e:
+                    assert len(core(exit, *[(s.pop(), e,) for s in stacks])) == len(stacks), 'parallel exit error'
+                    raise
+                else:
+                    assert len(core(exit, *[(s.pop(), None,) for s in stacks])) == len(stacks), 'parallel exit error'
+        return withas(_parallel)
+
+    # 分支流程
+    @classmethod
+    def branch(parent, child, pipe=lambda o: Arguments(o), core=tree(0)):
+        """
+        pipe: object -> Arguments
+        """
+        parent = withas(parent)
+        child = withas(child)
+
+        def enter(args):
+            call = args[0]
+            arguments = args[1]
+            obj = arguments(call)
+            return [obj, obj.__enter__(), ]
+
+        def exit(args):
+            obj = args[0]
+            e = args[1]
+            if e is None:
+                return obj.__exit__(None, None, None)
+            else:
+                return obj.__exit__(type(e), e, e.__traceback__)
+
+        def _branch(*args, **kwargs):
+            y = False
+            pstack = (lambda i: [i, i.__enter__(), ])(parent(*args, **kwargs))
+            try:
+                try:
+                    p = pstack.pop()
+                    assert isinstance(p, collections.Iterable), 'branch init error'
+                    p = list(p)
+                    l = len(p)
+                    pstack.append(p)
+                    p = None
+                except BaseException:
+                    p = None
+                    raise
+                else:
+                    cstacks = core(enter, *[(child, pipe(o),) for o in pstack.pop()])
+                    try:
+                        assert len(cstacks) == l, 'branch enter error'
+                    except BaseException:
+                        for s in cstacks:
+                            s.pop()
+                        assert len(core(exit, *[(s.pop(), None,) for s in cstacks])) == len(cstacks), 'branch exit error'
+                        raise
+                    else:
+                        try:
+                            y = True
+                            yield [s.pop() for s in cstacks]
+                        except BaseException as e:
+                            assert len(core(exit, *[(s.pop(), e,) for s in cstacks])) == len(cstacks), 'branch exit error'
+                            raise
+                        else:
+                            assert len(core(exit, *[(s.pop(), None,) for s in cstacks])) == len(cstacks), 'branch exit error'
+            except BaseException as e:
+                if pstack.pop().__exit__(type(e), e, e.__traceback__) is not True:
+                    raise
+                if not y:
+                    yield []
+            else:
+                pstack.pop().__exit__(None, None, None)
+        return withas(_branch)
+
+    # 合并流程
+    @classmethod
+    def merge(husband, wife, pipe):
+        """
+        pipe: *args, **kwargs -> bool
+        """
+        husband = withas(husband)
+        wife = withas(wife)
+
+        def _merge(*args, **kwargs):
+            stack = (lambda i: [i, i.__enter__(), ])((husband if not pipe(*args, **kwargs) else wife)(*args, **kwargs))
+            try:
+                yield stack.pop()
+            except BaseException as e:
+                if stack.pop().__exit__(type(e), e, e.__traceback__) is not True:
+                    raise
+            else:
+                stack.pop().__exit__(None, None, None)
+        return withas(_merge)
 
 
 class Stack:
@@ -11,18 +270,15 @@ class Stack:
     @ilock()
     def push(self, data):
         key = threading.currentThread().name
-        value = self.values.get(key)
-        if value is None:
-            value = []
-            self.values[key] = value
-        value.append(data)
+        if key not in self.values:
+            self.values[key] = []
+        self.values[key].append(data)
 
     # 出栈
     @ilock()
     def pop(self):
         key = threading.currentThread().name
-        value = self.values.get(key)
-        return value.pop()
+        return self.values[key].pop()
 
 
 class Arguments:
@@ -30,274 +286,14 @@ class Arguments:
         self.args = list(args)
         self.kwargs = dict(kwargs)
 
-    # 追加不定长参数（必备参数）
+    # 追加不定长参数
     def extend(self, *args):
         self.args.extend(args)
-        return self
 
-    # 更新关键字参数（默认参数）
+    # 更新关键字参数
     def update(self, **kwargs):
         self.kwargs.update(kwargs)
-        return self
 
     # 调用函数
-    def __call__(self, callable):
-        return callable(*self.args, **self.kwargs)
-
-
-class _Class:
-    # 输入转换
-    @classmethod
-    def input(cls, pipe=None):
-        """
-        pipe: *args, **kwargs -> Arguments
-        """
-        pipe = pipe if pipe is not None else lambda *args, **kwargs: Arguments(*args, **kwargs)
-
-        class Parent:
-            def __init__(self, *args, **kwargs):
-                self.args = args
-                self.kwargs = kwargs
-
-            def __enter__(self):
-                return self
-
-            def __exit__(self, t, v, tb):
-                pass
-        return _series(Parent, cls, lambda self: pipe(*self.args, **self.kwargs))
-
-    # 输出转换
-    @classmethod
-    def output(cls, pipe=None):
-        """
-        pipe: object -> object
-        """
-        pipe = pipe if pipe is not None else lambda o: o
-
-        class Child:
-            def __init__(self, *args, **kwargs):
-                self.args = args
-                self.kwargs = kwargs
-
-            def __enter__(self):
-                return pipe(self.args[0])
-
-            def __exit__(self, t, v, tb):
-                pass
-        return _series(cls, Child, lambda o: Arguments(o))
-
-    # 串联流程
-    @classmethod
-    def series(cls, child, pipe=None):
-        """
-        pipe: object -> Arguments
-        """
-        pipe = pipe if pipe is not None else lambda o: Arguments(o)
-        return _series(cls, child, pipe)
-
-    # 并联流程
-    @classmethod
-    def parallel(cls, *wives, pipe=None):
-        """
-        pipe: *args, **kwargs -> Arguments[]
-        """
-        l = len(wives)
-        pipe = pipe if pipe is not None else lambda *args, **kwargs: [Arguments(*args, **kwargs) for i in range(l + 1)]
-
-        def pipes(i):
-            def p(*args, **kwargs):
-                if i == l - 1:
-                    result = pipe(*args, **kwargs)
-                else:
-                    result = args[0]
-                if i == 0:
-                    return (result[i], result[i + 1],)
-                else:
-                    return (Arguments(result), result[i + 1],)
-            return p
-        cls = cls.output(lambda o: (o,))
-        for i in range(l):
-            cls = _parallel(cls, wives[i], pipes(i))
-            cls = cls.output(lambda o: o[0] + (o[1],))
-        return cls
-
-    # 分支流程
-    @classmethod
-    def branch(cls, child, pipe=None):
-        """
-        pipe: object -> Arguments
-        """
-        pipe = pipe if pipe is not None else lambda o: Arguments(o)
-        return _branch(cls, child, pipe)
-
-    # 合并流程
-    @classmethod
-    def merge(cls, wife, pipe=None):
-        """
-        pipe: *args, **kwargs -> bool
-        """
-        pipe = pipe if pipe is not None else lambda *args, **kwargs: False
-        return _merge(cls, wife, pipe)
-
-
-def _series(Parent, Child, Pipe):
-    class Series(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            self.__parent = Stack()
-            self.__child = Stack()
-
-        def __enter__(self):
-            parent = Parent(*self.args, **self.kwargs)
-            p = parent.__enter__()
-            try:
-                child = Pipe(p)(Child)
-                c = child.__enter__()
-                self.__parent.push(parent)
-                self.__child.push(child)
-                return c
-            except BaseException as e:
-                parent.__exit__(type(e), e, e.__traceback__)
-                raise
-
-        def __exit__(self, t, v, tb):
-            child = self.__child.pop()
-            parent = self.__parent.pop()
-            try:
-                child.__exit__(t, v, tb)
-            except BaseException as e:
-                parent.__exit__(type(e), e, e.__traceback__)
-                raise
-            parent.__exit__(t, v, tb)
-    return Series
-
-
-def _parallel(Husband, Wife, Pipe):
-    class Parallel(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            self.__husband = Stack()
-            self.__wife = Stack()
-
-        def __enter__(self):
-            arguments = Pipe(*self.args, **self.kwargs)
-            husband = arguments[0](Husband)
-            h = husband.__enter__()
-            try:
-                wife = arguments[1](Wife)
-                w = wife.__enter__()
-                self.__husband.push(husband)
-                self.__wife.push(wife)
-                return (h, w,)
-            except BaseException as e:
-                husband.__exit__(type(e), e, e.__traceback__)
-                raise
-
-        def __exit__(self, t, v, tb):
-            wife = self.__wife.pop()
-            husband = self.__husband.pop()
-            try:
-                wife.__exit__(t, v, tb)
-            except BaseException as e:
-                husband.__exit__(type(e), e, e.__traceback__)
-                raise
-            husband.__exit__(t, v, tb)
-    return Parallel
-
-
-def _branch(Parent, Child, Pipe):
-    def exits(queue, t, v, tb):
-        if queue:
-            exit = queue.pop()
-            try:
-                exit.__exit__(t, v, tb)
-            except BaseException as e:
-                exits(queue, type(e), e, e.__traceback__)
-                raise
-            exits(queue, t, v, tb)
-
-    class Branch(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            self.__parent = Stack()
-            self.__children = Stack()
-
-        def __enter__(self):
-            parent = Parent(*self.args, **self.kwargs)
-            ps = parent.__enter__()
-            children = []
-            cs = []
-            try:
-                for p in ps:
-                    child = Pipe(p)(Child)
-                    cs.append(child.__enter__())
-                    children.append(child)
-            except BaseException as e:
-                exits([parent] + children, type(e), e, e.__traceback__)
-                raise
-            self.__parent.push(parent)
-            self.__children.push(children)
-            return tuple(cs)
-
-        def __exit__(self, t, v, tb):
-            children = self.__children.pop()
-            parent = self.__parent.pop()
-            exits([parent] + children, t, v, tb)
-    return Branch
-
-
-def _merge(Husband, Wife, Pipe):
-    class Merge(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            self.__obj = Stack()
-
-        def __enter__(self):
-            obj = (Wife if Pipe(*self.args, **self.kwargs) else Husband)(*self.args, **self.kwargs)
-            o = obj.__enter__()
-            self.__obj.push(obj)
-            return o
-
-        def __exit__(self, t, v, tb):
-            obj = self.__obj.pop()
-            obj.__exit__(t, v, tb)
-    return Merge
-
-
-# 追加（装饰器）
-def extends(Class):
-    class Extends(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-            self.__obj = Stack()
-
-        def __enter__(self):
-            obj = Class(*self.args, **self.kwargs)
-            o = obj.__enter__()
-            self.__obj.push(obj)
-            return o
-
-        def __exit__(self, t, v, tb):
-            obj = self.__obj.pop()
-            obj.__exit__(t, v, tb)
-    return Extends
-
-
-# 更新（装饰器）
-def updates(Def):
-    class Updates(_Class):
-        def __init__(self, *args, **kwargs):
-            self.args = args
-            self.kwargs = kwargs
-
-        def __enter__(self):
-            return Def(*self.args, **self.kwargs)
-
-        def __exit__(self, t, v, tb):
-            pass
-    return Updates
+    def __call__(self, call):
+        return call(*self.args, **self.kwargs)
