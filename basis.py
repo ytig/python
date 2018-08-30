0e26179a9aff27d245554bd67ea45a895ba3b550
ZhangLiang <qimuyangtao@gmail.com>
2018-08-30 23:27:33 +0800

    z

--- basis.py
+++ basis.py
@@ -1,56 +1,71 @@
 #!/usr/local/bin/python3
+import collections
+
+
 class uint:
+    @staticmethod
+    def new(generics):
+        if isinstance(generics, uint):
+            return generics
+        else:
+            return uint(generics)
+
     def __init__(self, generics):
         if isinstance(generics, int):
             self.value = generics & 0xFFFFFFFF
-        else:
+        elif isinstance(generics, bytes):
+            self.value = int.from_bytes(generics, 'little') & 0xFFFFFFFF
+        elif isinstance(generics, str):
+            self.value = int.from_bytes(bytes(generics, 'ascii'), 'little') & 0xFFFFFFFF
+        elif isinstance(generics, collections.Iterable):
             self.value = int.from_bytes(bytes(generics), 'little') & 0xFFFFFFFF
+        raise TypeError
 
     def __add__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value + other.value)
 
     def __sub__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value - other.value)
 
     def __mul__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value * other.value)
 
     def __div__(self, other):
         return self // other
 
     def __floordiv__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value // other.value)
 
     def __mod__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value % other.value)
 
     def __pow__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value**other.value)
 
     def __lshift__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value << other.value)
 
     def __rshift__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value >> other.value)
 
     def __and__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value & other.value)
 
     def __xor__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value ^ other.value)
 
     def __or__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return uint(self.value | other.value)
 
     def __iaddr__(self, other):
@@ -89,28 +104,64 @@ class uint:
     def __ior__(self, other):
         return self | other
 
+    def __raddr__(self, other):
+        return uint(other) + self
+
+    def __rsub__(self, other):
+        return uint(other) - self
+
+    def __rmul__(self, other):
+        return uint(other) * self
+
+    def __rdiv__(self, other):
+        return uint(other) / self
+
+    def __rfloordiv__(self, other):
+        return uint(other) // self
+
+    def __rmod__(self, other):
+        return uint(other) % self
+
+    def __rpow__(self, other):
+        return uint(other)**self
+
+    def __rlshift__(self, other):
+        return uint(other) << self
+
+    def __rrshift__(self, other):
+        return uint(other) >> self
+
+    def __rand__(self, other):
+        return uint(other) & self
+
+    def __rxor__(self, other):
+        return uint(other) ^ self
+
+    def __ror__(self, other):
+        return uint(other) | self
+
     def __eq__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value == other.value
 
     def __ne__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value != other.value
 
     def __gt__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value > other.value
 
     def __lt__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value < other.value
 
     def __ge__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value >= other.value
 
     def __le__(self, other):
-        assert isinstance(other, uint)
+        other = uint.new(other)
         return self.value <= other.value
 
     def __len__(self):
