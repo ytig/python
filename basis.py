6a904dac38d3776247915b2ffb0cb6ed5f01d061
ZhangLiang <qimuyangtao@gmail.com>
2018-08-31 00:43:19 +0800

    _int

--- basis.py
+++ basis.py
@@ -2,74 +2,124 @@
 import collections
 
 
-class uint8:
-    @staticmethod
-    def make(generics):
-        if isinstance(generics, uint8):
+class _int:
+    # 类型转换
+    @classmethod
+    def format(cls, generics):
+        if type(generics) is cls:
             return generics
         else:
-            return uint8(generics)
+            return cls(generics)
 
-    def __init__(self, generics):
-        if isinstance(generics, int):
-            self.value = generics & 0xFF
-        elif isinstance(generics, bytes):
-            self.value = int.from_bytes(generics, 'little') & 0xFF
-        elif isinstance(generics, str):
-            self.value = int.from_bytes(bytes(generics, 'ascii'), 'little') & 0xFF
-        elif isinstance(generics, collections.Iterable):
-            self.value = int.from_bytes(bytes(generics), 'little') & 0xFF
-        else:
-            raise TypeError
+    # 运算优先级
+    @classmethod
+    def priority(cls):
+        return -1
+
+    def __init__(self, value):
+        self.value = value
+
+    def __neg__(self):
+        return type(self)(-self.value)
+
+    def __pos__(self):
+        return type(self)(+self.value)
+
+    def __invert__(self):
+        return type(self)(~self.value)
 
     def __add__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value + other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__radd__(self)
+        else:
+            return tp1(self.value + tp1.format(other).value)
 
     def __sub__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value - other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rsub__(self)
+        else:
+            return tp1(self.value - tp1.format(other).value)
 
     def __mul__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value * other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rmul__(self)
+        else:
+            return tp1(self.value * tp1.format(other).value)
 
     def __div__(self, other):
         return self // other
 
     def __floordiv__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value // other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rfloordiv__(self)
+        else:
+            return tp1(self.value // tp1.format(other).value)
 
     def __mod__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value % other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rmod__(self)
+        else:
+            return tp1(self.value % tp1.format(other).value)
 
     def __pow__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value**other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rpow__(self)
+        else:
+            return tp1(self.value ** tp1.format(other).value)
 
     def __lshift__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value << other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rlshift__(self)
+        else:
+            return tp1(self.value << tp1.format(other).value)
 
     def __rshift__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value >> other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rrshift__(self)
+        else:
+            return tp1(self.value >> tp1.format(other).value)
 
     def __and__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value & other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rand__(self)
+        else:
+            return tp1(self.value & tp1.format(other).value)
 
     def __xor__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value ^ other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__rxor__(self)
+        else:
+            return tp1(self.value ^ tp1.format(other).value)
 
     def __or__(self, other):
-        other = uint8.make(other)
-        return uint8(self.value | other.value)
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return other.__ror__(self)
+        else:
+            return tp1(self.value | tp1.format(other).value)
 
-    def __iaddr__(self, other):
+    def __iadd__(self, other):
         return self + other
 
     def __isub__(self, other):
@@ -88,7 +138,7 @@ class uint8:
         return self % other
 
     def __ipow__(self, other):
-        return self**other
+        return self ** other
 
     def __ilshift__(self, other):
         return self << other
@@ -106,267 +156,145 @@ class uint8:
         return self | other
 
     def __radd__(self, other):
-        return uint8(other) + self
+        return type(self)(other) + self
 
     def __rsub__(self, other):
-        return uint8(other) - self
+        return type(self)(other) - self
 
     def __rmul__(self, other):
-        return uint8(other) * self
+        return type(self)(other) * self
 
     def __rdiv__(self, other):
-        return uint8(other) / self
+        return type(self)(other) / self
 
     def __rfloordiv__(self, other):
-        return uint8(other) // self
+        return type(self)(other) // self
 
     def __rmod__(self, other):
-        return uint8(other) % self
+        return type(self)(other) % self
 
     def __rpow__(self, other):
-        return uint8(other)**self
+        return type(self)(other) ** self
 
     def __rlshift__(self, other):
-        return uint8(other) << self
+        return type(self)(other) << self
 
     def __rrshift__(self, other):
-        return uint8(other) >> self
+        return type(self)(other) >> self
 
     def __rand__(self, other):
-        return uint8(other) & self
+        return type(self)(other) & self
 
     def __rxor__(self, other):
-        return uint8(other) ^ self
+        return type(self)(other) ^ self
 
     def __ror__(self, other):
-        return uint8(other) | self
+        return type(self)(other) | self
 
     def __eq__(self, other):
-        other = uint8.make(other)
-        return self.value == other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value == other.value
+        else:
+            return self.value == tp1.format(other).value
 
     def __ne__(self, other):
-        other = uint8.make(other)
-        return self.value != other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value != other.value
+        else:
+            return self.value != tp1.format(other).value
 
     def __gt__(self, other):
-        other = uint8.make(other)
-        return self.value > other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value > other.value
+        else:
+            return self.value > tp1.format(other).value
 
     def __lt__(self, other):
-        other = uint8.make(other)
-        return self.value < other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value < other.value
+        else:
+            return self.value < tp1.format(other).value
 
     def __ge__(self, other):
-        other = uint8.make(other)
-        return self.value >= other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value >= other.value
+        else:
+            return self.value >= tp1.format(other).value
 
     def __le__(self, other):
-        other = uint8.make(other)
-        return self.value <= other.value
+        tp1 = type(self)
+        tp2 = type(other)
+        if isinstance(other, _int) and tp2.priority() > tp1.priority():
+            return tp2.format(self).value <= other.value
+        else:
+            return self.value <= tp1.format(other).value
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        return self.value.to_bytes(len(self), 'little')[index]
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class uint8(_int):
+    @classmethod
+    def priority(cls):
+        return 0
+
+    def __init__(self, generics):
+        if isinstance(generics, _int):
+            value = generics.value
+        elif isinstance(generics, int):
+            value = generics
+        elif isinstance(generics, bytes):
+            value = int.from_bytes(generics, 'little')
+        elif isinstance(generics, str):
+            value = int.from_bytes(bytes(generics, 'ascii'), 'little')
+        elif isinstance(generics, collections.Iterable):
+            value = int.from_bytes(bytes(generics), 'little')
+        else:
+            raise TypeError
+        super().__init__(value & 0xFF)
 
     def __len__(self):
         return 1
 
-    def __getitem__(self, index):
-        return self.value.to_bytes(1, 'little')[index]
 
-    def __neg__(self):
-        return uint8(-self.value)
-
-    def __pos__(self):
-        return uint8(+self.value)
-
-    def __invert__(self):
-        return uint8(~self.value)
-
-    def __str__(self):
-        return str(self.value)
-
-    def __repr__(self):
-        return str(self.value)
-
-
-class uint:
-    @staticmethod
-    def make(generics):
-        if isinstance(generics, uint):
-            return generics
-        else:
-            return uint(generics)
+class uint(_int):
+    @classmethod
+    def priority(cls):
+        return 1
 
     def __init__(self, generics):
-        if isinstance(generics, int):
-            self.value = generics & 0xFFFFFFFF
+        if isinstance(generics, _int):
+            value = generics.value
+        elif isinstance(generics, int):
+            value = generics
         elif isinstance(generics, bytes):
-            self.value = int.from_bytes(generics, 'little') & 0xFFFFFFFF
+            value = int.from_bytes(generics, 'little')
         elif isinstance(generics, str):
-            self.value = int.from_bytes(bytes(generics, 'ascii'), 'little') & 0xFFFFFFFF
+            value = int.from_bytes(bytes(generics, 'ascii'), 'little')
         elif isinstance(generics, collections.Iterable):
-            self.value = int.from_bytes(bytes(generics), 'little') & 0xFFFFFFFF
+            value = int.from_bytes(bytes(generics), 'little')
         else:
             raise TypeError
-
-    def __add__(self, other):
-        other = uint.make(other)
-        return uint(self.value + other.value)
-
-    def __sub__(self, other):
-        other = uint.make(other)
-        return uint(self.value - other.value)
-
-    def __mul__(self, other):
-        other = uint.make(other)
-        return uint(self.value * other.value)
-
-    def __div__(self, other):
-        return self // other
-
-    def __floordiv__(self, other):
-        other = uint.make(other)
-        return uint(self.value // other.value)
-
-    def __mod__(self, other):
-        other = uint.make(other)
-        return uint(self.value % other.value)
-
-    def __pow__(self, other):
-        other = uint.make(other)
-        return uint(self.value**other.value)
-
-    def __lshift__(self, other):
-        other = uint.make(other)
-        return uint(self.value << other.value)
-
-    def __rshift__(self, other):
-        other = uint.make(other)
-        return uint(self.value >> other.value)
-
-    def __and__(self, other):
-        other = uint.make(other)
-        return uint(self.value & other.value)
-
-    def __xor__(self, other):
-        other = uint.make(other)
-        return uint(self.value ^ other.value)
-
-    def __or__(self, other):
-        other = uint.make(other)
-        return uint(self.value | other.value)
-
-    def __iaddr__(self, other):
-        return self + other
-
-    def __isub__(self, other):
-        return self - other
-
-    def __imul__(self, other):
-        return self * other
-
-    def __idiv__(self, other):
-        return self / other
-
-    def __ifloordiv__(self, other):
-        return self // other
-
-    def __imod__(self, other):
-        return self % other
-
-    def __ipow__(self, other):
-        return self**other
-
-    def __ilshift__(self, other):
-        return self << other
-
-    def __irshift__(self, other):
-        return self >> other
-
-    def __iand__(self, other):
-        return self & other
-
-    def __ixor__(self, other):
-        return self ^ other
-
-    def __ior__(self, other):
-        return self | other
-
-    def __radd__(self, other):
-        return uint(other) + self
-
-    def __rsub__(self, other):
-        return uint(other) - self
-
-    def __rmul__(self, other):
-        return uint(other) * self
-
-    def __rdiv__(self, other):
-        return uint(other) / self
-
-    def __rfloordiv__(self, other):
-        return uint(other) // self
-
-    def __rmod__(self, other):
-        return uint(other) % self
-
-    def __rpow__(self, other):
-        return uint(other)**self
-
-    def __rlshift__(self, other):
-        return uint(other) << self
-
-    def __rrshift__(self, other):
-        return uint(other) >> self
-
-    def __rand__(self, other):
-        return uint(other) & self
-
-    def __rxor__(self, other):
-        return uint(other) ^ self
-
-    def __ror__(self, other):
-        return uint(other) | self
-
-    def __eq__(self, other):
-        other = uint.make(other)
-        return self.value == other.value
-
-    def __ne__(self, other):
-        other = uint.make(other)
-        return self.value != other.value
-
-    def __gt__(self, other):
-        other = uint.make(other)
-        return self.value > other.value
-
-    def __lt__(self, other):
-        other = uint.make(other)
-        return self.value < other.value
-
-    def __ge__(self, other):
-        other = uint.make(other)
-        return self.value >= other.value
-
-    def __le__(self, other):
-        other = uint.make(other)
-        return self.value <= other.value
+        super().__init__(value & 0xFFFFFFFF)
 
     def __len__(self):
         return 4
-
-    def __getitem__(self, index):
-        return self.value.to_bytes(4, 'little')[index]
-
-    def __neg__(self):
-        return uint(-self.value)
-
-    def __pos__(self):
-        return uint(+self.value)
-
-    def __invert__(self):
-        return uint(~self.value)
-
-    def __str__(self):
-        return str(self.value)
-
-    def __repr__(self):
-        return str(self.value)
