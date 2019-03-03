5d69f70f54e1a656740ecd16d0b7ea08c067a040
ZhangLiang <qimuyangtao@gmail.com>
2019-03-03 15:23:01 +0800

    new loop

--- loop.py
+++ loop.py
@@ -2,31 +2,30 @@
 import time
 import threading
 import itertools
-from decorator import Lock, ilock
+from decorator import Lock, ilock, ithrow
 from ab import weakmethod
 from shutdown import bregister, unregister
 from logger import loge
 
 
-class Loop(threading.Thread):
-    def __init__(self, daemon=False):
-        super().__init__(daemon=daemon)
-        self._targets = []
-        self._count = itertools.count(1)
-        self._wait = threading.Event()
-        self._closer = threading.Event()
-        self._closed = threading.Event()
-        self.__shutdown__ = weakmethod(self, 'shutdown')
-        bregister(self.__shutdown__)
+class Loop:
+    def __init__(self):
+        self.targets = []
+        self.count = itertools.count(1)
+        self.wait = threading.Event()
+        self.opened = threading.Event()
+        self.closer = threading.Event()
+        self.closed = threading.Event()
 
     # 执行
     @ilock()
     def post(self, target, args=(), kwargs={}, log=loge, delay=0, tag=''):
+        assert not self.closed.is_set(), 'loop has been closed'
         until = time.monotonic() + max(delay, 0)
-        pid = next(self._count)
-        if not self._targets or until < self._targets[0]['until']:
-            self._wait.set()
-        self._targets.append({
+        pid = next(self.count)
+        if not self.targets or until < self.targets[0]['until']:
+            self.wait.set()
+        self.targets.append({
             'until': until,
             'pid': pid,
             'tag': tag,
@@ -35,50 +34,46 @@ class Loop(threading.Thread):
             'kwargs': kwargs,
             'log': log,
         })
-        self._targets.sort(key=lambda d: (d['until'], d['pid'],))
-        if not self.is_alive():
-            self.start()
+        self.targets.sort(key=lambda d: (d['until'], d['pid'],))
         return pid
 
     # 移除
     @ilock()
     def remove(self, generics):
         ret = 0
-        for i in range(len(self._targets) - 1, -1, -1):
+        for i in range(len(self.targets) - 1, -1, -1):
             p = False
             if generics is None:
                 p = True
             elif isinstance(generics, int):
-                p = self._targets[i]['pid'] == generics
+                p = self.targets[i]['pid'] == generics
             elif isinstance(generics, str):
-                p = self._targets[i]['tag'] == generics
+                p = self.targets[i]['tag'] == generics
             elif callable(generics):
-                p = self._targets[i]['target'] is generics
+                p = self.targets[i]['target'] is generics
             if p:
-                self._targets.pop(i)
+                self.targets.pop(i)
                 ret += 1
         return ret
 
-    # 终止
-    def shutdown(self):
-        unregister(self.__shutdown__)
-        self._closer.set()
-        self._wait.set()
-        self._closed.wait()
-
-    def run(self):
+    # 开始
+    def enter(self):
+        with Lock(self):
+            assert not self.opened.is_set(), 'loop has been opened'
+            self.opened.set()
         while True:
             pop = None
             timeout = None
             with Lock(self):
-                if self._targets:
-                    timeout = self._targets[0]['until'] - time.monotonic()
+                if self.targets:
+                    timeout = self.targets[0]['until'] - time.monotonic()
                     if timeout <= 0:
-                        pop = self._targets.pop(0)
-                elif self._closer.is_set():
+                        pop = self.targets.pop(0)
+                elif self.closer.is_set():
+                    self.closed.set()
                     break
                 if pop is None:
-                    self._wait.clear()
+                    self.wait.clear()
             if pop is not None:
                 try:
                     pop['target'](*pop['args'], **pop['kwargs'])
@@ -88,18 +83,43 @@ class Loop(threading.Thread):
                     except BaseException:
                         pass
             else:
-                self._wait.wait(timeout=timeout)
-        self._closed.set()
+                self.wait.wait(timeout=timeout)
+
+    # 退出
+    def exit(self):
+        with Lock(self):
+            self.closer.set()
+            if not self.opened.is_set() and not self.targets:
+                self.closed.set()
+        self.wait.set()
 
 
-main_loop = Loop()  # 主循环
+class LoopThread(threading.Thread):
+    def __init__(self, daemon=None):
+        super().__init__(daemon=daemon)
+        self.loop = Loop()
+        self.__shutdown__ = weakmethod(self, 'shutdown')
+        bregister(self.__shutdown__)
 
+    # 执行
+    def post(self, *args, **kwargs):
+        pid = self.loop.post(*args, **kwargs)
+        self.start()
+        return pid
 
-# 执行
-def post(*args, **kwargs):
-    return main_loop.post(*args, **kwargs)
+    # 移除
+    def remove(self, *args, **kwargs):
+        return self.loop.remove(*args, **kwargs)
 
+    # 终止
+    def shutdown(self):
+        unregister(self.__shutdown__)
+        self.loop.exit()
 
-# 移除
-def remove(*args, **kwargs):
-    return main_loop.remove(*args, **kwargs)
+    @ilock()
+    @ithrow()
+    def start(self):
+        return super().start()
+
+    def run(self):
+        self.loop.enter()
