abaf57e7ae1318cd2fd9170bc2940e7ba46082f4
ZhangLiang <qimuyangtao@gmail.com>
2019-03-12 14:45:35 +0800

    inquirer

---
+++ _inquirer.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+import collections
+import inquirer
+
+
+# 选择
+def choose(message, choices, default=None):
+    if isinstance(choices, (tuple, list,)):
+        return inquirer.prompt([inquirer.List('', message=message, choices=choices, default=default)], raise_keyboard_interrupt=True)['']
+    elif isinstance(choices, collections.OrderedDict):
+        return choices[choose(message, list(choices), default=default)]
+    raise TypeError
