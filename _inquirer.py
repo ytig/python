6f28e45b5c597d6fd6b1975e86d5bdf0424ea54d
ZhangLiang <qimuyangtao@gmail.com>
2019-03-12 14:52:47 +0800

    z

--- _inquirer.py
+++ _inquirer.py
@@ -6,6 +6,7 @@ import inquirer
 # 选择
 def choose(message, choices, default=None):
     if isinstance(choices, (tuple, list,)):
+        assert choices
         return inquirer.prompt([inquirer.List('', message=message, choices=choices, default=default)], raise_keyboard_interrupt=True)['']
     elif isinstance(choices, collections.OrderedDict):
         return choices[choose(message, list(choices), default=default)]
