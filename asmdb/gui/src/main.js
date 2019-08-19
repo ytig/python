1818f7bcb71aaec0c69b512dabf42887f0b567a8
ZhangLiang <qimuyangtao@gmail.com>
2019-08-19 23:06:17 +0800

    z

--- asmdb/gui/src/main.js
+++ asmdb/gui/src/main.js
@@ -1,15 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import Connect from './Connect';
+import Debug from './Debug';
 
-/* eslint-disable no-new */
+Vue.use(VueRouter);
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  router: new VueRouter({
+    routes: [{
+      path: '/',
+      redirect: '/connect',
+    }, {
+      path: '/connect',
+      component: Connect,
+    }, {
+      path: '/debug',
+      component: Debug,
+    }],
+  }),
+  template: '<router-view/>',
+});
