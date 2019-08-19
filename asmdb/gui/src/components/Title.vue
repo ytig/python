1a2e9d5af6c9fa6087a282de71fc3d7c52f7d082
ZhangLiang <qimuyangtao@gmail.com>
2019-08-19 23:54:01 +0800

    Title

--- asmdb/gui/src/components/Title.vue
+++ asmdb/gui/src/components/Title.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "Memory"
+  name: "Title"
 };
 </script>
 
---
+++ asmdb/gui/src/windows/Python3.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Python3"
+};
+</script>
+
+<style>
+</style>
