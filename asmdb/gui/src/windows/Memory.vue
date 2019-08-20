eae6e09f1fbbd9e19eb2cda8a00abc5620c0c672
ZhangLiang <qimuyangtao@gmail.com>
2019-08-20 22:10:39 +0800

    z

--- asmdb/gui/src/windows/Memory.vue
+++ asmdb/gui/src/windows/Memory.vue
@@ -2,7 +2,9 @@
   <div class="memory-container">
     <Title :name="'Memory'"></Title>
     <div v-for="i in Array(40)" :key="i">
-      <span class="todo">bf001234 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f ........aaaaaaaa</span>
+      <div
+        class="todo"
+      >0xbf001234 00 01 02 03 04 05 06 07 &nbsp;08 09 0a 0b 0c 0d 0e 0f &nbsp;........aaaaaaaa</div>
     </div>
   </div>
 </template>
@@ -16,6 +18,7 @@ export default {};
 
 .memory-container {
   .todo {
+    padding: 0px 12px;
     color: #abb2bf;
     font-size: 12px;
   }
--- asmdb/gui/src/windows/Stack.vue
+++ asmdb/gui/src/windows/Stack.vue
@@ -2,7 +2,7 @@
   <div class="stack-container">
     <Title :name="'Stack'"></Title>
     <div v-for="i in Array(40)" :key="i">
-      <span class="todo">1234 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f</span>
+      <div class="todo">1234 00 01 02 03 04 05 06 07 &nbsp;08 09 0a 0b 0c 0d 0e 0f</div>
     </div>
   </div>
 </template>
@@ -16,6 +16,7 @@ export default {};
 
 .stack-container {
   .todo {
+    padding: 0px 12px;
     color: #abb2bf;
     font-size: 12px;
   }
