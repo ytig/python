6f045016b48d89ee9bb7e4ddbd65c938d4c960fd
ZhangLiang <qimuyangtao@gmail.com>
2019-08-20 23:14:40 +0800

    indicator

--- asmdb/gui/src/windows/Stack.vue
+++ asmdb/gui/src/windows/Stack.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="stack-container">
     <Title :name="'Stack'"></Title>
-    <div v-for="i in Array(40)" :key="i">
-      <div class="todo">1234 &nbsp;00 01 02 03 04 05 06 07 &nbsp;08 09 0a 0b 0c 0d 0e 0f</div>
+    <div v-for="i in Array(38)" :key="i">
+      <div class="todo">+0x190 &nbsp;00 01 02 03 04 05 06 07</div>
+    </div>
+    <div class="indicator">
+      <div v-for="(item, index) in Array(10)" :key="index">{{index}}</div>
     </div>
   </div>
 </template>
@@ -15,10 +18,31 @@ export default {};
 @import "~@/styles/theme.less";
 
 .stack-container {
+  display: flex;
+  flex-direction: column;
   .todo {
     padding: 0px 12px;
     color: #abb2bf;
     font-size: 12px;
+  }
+  .indicator {
+    flex-grow: 1;
+    display: flex;
+    align-items: flex-end;
+    > *:first-child {
+      border-left: 0px;
+    }
+    > * {
+      flex-grow: 1;
+      height: 40px;
+      line-height: 40px;
+      background: @color-dark-background;
+      border-top: 1px solid @color-border;
+      border-left: 1px solid @color-border;
+      text-align: center;
+      color: #abb2bf;
+      font-size: 12px;
+    }
   }
 }
 </style>
