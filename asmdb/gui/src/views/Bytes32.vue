7fce5549694e3b77c380569d82f2ccf66c2d00b9
ZhangLiang <qimuyangtao@gmail.com>
2019-08-21 23:35:29 +0800

    z

--- asmdb/gui/src/views/Bytes32.vue
+++ asmdb/gui/src/views/Bytes32.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="bytes32-container"></div>
+  <div class="bytes32-container">
+    <span :class="!sameAt(3)?'bytes32-changed':''">{{hexAt(3)}}</span>
+    <span>&nbsp;</span>
+    <span :class="!sameAt(2)?'bytes32-changed':''">{{hexAt(2)}}</span>
+    <span>&nbsp;</span>
+    <span :class="!sameAt(1)?'bytes32-changed':''">{{hexAt(1)}}</span>
+    <span>&nbsp;</span>
+    <span :class="!sameAt(0)?'bytes32-changed':''">{{hexAt(0)}}</span>
+  </div>
 </template>
 
 <script>
+function hexAt(int32, index) {
+  return "00";
+}
+
+function sameAt(int32, _int32, index) {
+  return false;
+}
+
 export default {
   props: {
     oldBytes: Number,
     newBytes: Number
+  },
+  methods: {
+    hexAt: function(index) {
+      return hexAt(this.newBytes, index);
+    },
+    sameAt: function(index) {
+      if (this.oldBytes == null) {
+        return true;
+      }
+      return sameAt(this.newBytes, this.oldBytes, index);
+    }
   }
 };
 </script>
@@ -15,5 +42,12 @@ export default {
 @import "~@/styles/theme.less";
 
 .bytes32-container {
+  > span {
+    color: @color-text;
+    font-size: 12px;
+  }
+  > span.bytes32-changed {
+    color: @color-diff-text;
+  }
 }
 </style>
