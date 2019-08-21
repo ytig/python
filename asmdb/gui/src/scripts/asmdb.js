1ef674644e46eb0bbb1de8b00969396a3ed257c3
ZhangLiang <qimuyangtao@gmail.com>
2019-08-21 16:59:49 +0800

    z

--- asmdb/gui/src/scripts/asmdb.js
+++ asmdb/gui/src/scripts/asmdb.js
@@ -1,8 +1,11 @@
 function registerEvent(type, object) {
   //todo
   setTimeout(() => {
-    object.onBreak(1, '');
+    object.onBreak(1, '00112233445566778899AABBCCDDEEFF');
   }, 1000);
+  setTimeout(() => {
+    object.onBreak(1, '00112233445566770099AABBCCDDEEFF');
+  }, 8000);
 }
 
 function unregisterEvent(object) {
