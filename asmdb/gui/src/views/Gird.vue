1e3fb0b59c13a3fdceee39045a1e29d010691677
ZhangLiang <qimuyangtao@gmail.com>
2019-08-21 21:56:56 +0800

    fix

--- asmdb/gui/src/views/Gird.vue
+++ asmdb/gui/src/views/Gird.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="gird-container">
-    <div v-for="i in Math.ceil(items.length/3)" :key="i">
-      <div v-for="j in column" :key="i*column+j" :style="{width:(100/column)+'%'}">
-        <slot v-if="i*column+j<items.length" :item="items[i*column+j]" :index="i*column+j"></slot>
+    <div v-for="i in Math.ceil(items.length/column)" :key="i-1">
+      <div v-for="j in column" :key="(i-1)*column+j-1" :style="{width:(100/column)+'%'}">
+        <slot v-if="(i-1)*column+j-1<items.length" :item="items[(i-1)*column+j-1]" :index="(i-1)*column+j-1"></slot>
       </div>
     </div>
   </div>
