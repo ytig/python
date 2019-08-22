43c3240959eca4e7a00d5d222ad70bf07984f7c6
ZhangLiang <qimuyangtao@gmail.com>
2019-08-22 16:58:18 +0800

    z

--- asmdb/gui/src/scripts/asmdb.js
+++ asmdb/gui/src/scripts/asmdb.js
@@ -5,12 +5,17 @@ function registerEvent(type, object) {
     for (var i = 0; i < 10 * 2 * 344; i++) {
       hex += "0123456789abcdef".charAt(Math.floor(16 * Math.random()));
     }
+    object.cache = hex;
     object.onBreak(1, hex);
   }, 1000);
   setTimeout(() => {
     var hex = "";
     for (var i = 0; i < 10 * 2 * 344; i++) {
-      hex += "0123456789abcdef".charAt(Math.floor(16 * Math.random()));
+      if (Math.random() > 0.98) {
+        hex += "0123456789abcdef".charAt(Math.floor(16 * Math.random()));
+      } else {
+        hex += object.cache.slice(i, i + 1);
+      }
     }
     object.onBreak(1, hex);
   }, 2000);
