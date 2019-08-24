b84f88c45d8eb57f466604d68266ca2e0a6134d6
ZhangLiang <qimuyangtao@gmail.com>
2019-08-25 00:21:35 +0800

    z

--- asmdb/gui/src/views/Navigation.vue
+++ asmdb/gui/src/views/Navigation.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="navigation-container">
-    <div :css-focus="focus" :css-disable="disable"></div>
     <span class="user-select-none" :css-focus="focus" :css-disable="disable">{{name}}</span>
   </div>
 </template>
@@ -19,30 +18,18 @@ export default {
 @import '~@/styles/theme.less';
 
 .navigation-container {
-  position: relative;
   height: 40px;
-  > div {
-    position: absolute;
-    border: 0px solid @color-text;
-    border-right-color: transparent !important;
-    border-bottom-color: transparent !important;
-  }
-  > div[css-focus='true'] {
-    border-width: 5px;
-  }
-  > div[css-disable='true'] {
-    border-color: @color-darker-text;
-  }
+  line-height: 40px;
+  padding-left: 12px;
   > span {
-    position: absolute;
-    left: 12px;
-    bottom: 5px;
-    font-size: 18px;
+    font-size: 16px;
     color: @color-text;
     font-family: 'Wawati SC';
   }
+  > span[css-focus='true'] {
+    color: @color-light-text;
+  }
   > span[css-disable='true'] {
-    color: @color-darker-text;
     text-decoration: line-through;
   }
 }
