b37aaa13440c68bd96a314d6d8e0305fb8591581
ZhangLiang <qimuyangtao@gmail.com>
2019-08-24 21:03:56 +0800

    f

--- asmdb/gui/src/windows/Memory.vue
+++ asmdb/gui/src/windows/Memory.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="memory-container" :style="{width:windowWidth+'px'}">
-    <Navigation :name="'Memory'" :disable="disable"></Navigation>
+  <div class="memory-container" :style="{width:windowWidth+'px'}" @mousedown="requestFocus">
+    <Navigation :name="'Memory'" :focus="focus" :disable="disable"></Navigation>
     <Empty v-if="items.length==0" :text="'[no data]'" style="padding-top:12px;"></Empty>
     <div v-else>
       <Bytes :value="{lineNumber:'0x00112233',newBytes:[1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8],showString:true}"></Bytes>
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import keyboard from '@/scripts/keyboard.js';
 import asmdb from '@/scripts/asmdb.js';
 const groupBy = 4; //4or8
 
@@ -19,6 +20,7 @@ function measureTextWidth(length) {
 export default {
   data: function() {
     return {
+      focus: false,
       disable: true,
       items: [],
       itemSelection: null,
@@ -37,10 +39,23 @@ export default {
     }
   },
   created: function() {
+    keyboard.registerWindow(this);
     asmdb.registerEvent('memory', this);
   },
   destroyed: function() {
     asmdb.unregisterEvent('memory', this);
+    keyboard.unregisterWindow(this);
+  },
+  methods: {
+    requestFocus: function() {
+      keyboard.requestFocus(this);
+    },
+    onFocusChanged: function(value) {
+      this.focus = value;
+    },
+    onKeyboardClick: function(event) {
+      //todo
+    }
   }
 };
 </script>
