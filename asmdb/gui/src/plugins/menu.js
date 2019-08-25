293e3f8c5f3eeba6e409b9d41250894cdce62e89
ZhangLiang <qimuyangtao@gmail.com>
2019-08-26 00:59:51 +0800

    z

--- asmdb/gui/src/plugins/menu.js
+++ asmdb/gui/src/plugins/menu.js
@@ -20,6 +20,7 @@ export default {
       if (intercept) {
         vm.onClick(event);
         event.stopPropagation();
+        intercept = false;
       }
     }, true);
     document.addEventListener('contextmenu', function (event) {
