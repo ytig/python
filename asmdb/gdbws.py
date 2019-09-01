0979157c23bde6f0984aa69c064ab87374de91aa
ZhangLiang <qimuyangtao@gmail.com>
2019-09-01 23:01:39 +0800

    rwlock

--- asmdb/gdbws.py
+++ asmdb/gdbws.py
@@ -7,30 +7,16 @@ SESSIONS = {}
 
 def onopen(token, emit):
     if token not in SESSIONS:
-        session = Session(token)
-        session._transfer = (asyncio.Lock(), [],)
-        SESSIONS[token] = session
-    session = SESSIONS[token]
-    session._transfer[1].append(session.onopen(emit))
-    asyncio.ensure_future(_transfer(*session._transfer))
+        SESSIONS[token] = Session(token)
+    asyncio.ensure_future(SESSIONS[token].onopen(emit))
 
 
 def onmessage(token, emit, data):
-    session = SESSIONS[token]
-    session._transfer[1].append(session.onmessage(emit, data))
-    asyncio.ensure_future(_transfer(*session._transfer))
+    asyncio.ensure_future(SESSIONS[token].onmessage(emit, data))
 
 
 def onclose(token, emit):
-    session = SESSIONS[token]
-    session._transfer[1].append(session.onclose(emit))
-    asyncio.ensure_future(_transfer(*session._transfer))
-
-
-async def _transfer(lock, coros):  # todo optimize lock
-    async with lock:
-        while coros:
-            await coros.pop(0)
+    asyncio.ensure_future(SESSIONS[token].onclose(emit))
 
 
 def suit_js(obj):
@@ -45,37 +31,72 @@ def suit_js(obj):
     return obj
 
 
+class RWLock:
+    def __init__(self):
+        self.lock = asyncio.Lock()
+        self.refs = 0
+
+    async def acquire_read(self):
+        self.refs += 1
+        if self.refs == 1:
+            await self.lock.acquire()
+
+    def release_read(self):
+        self.refs -= 1
+        if self.refs == 0:
+            self.lock.release()
+
+    async def acquire_write(self):
+        await self.lock.acquire()
+
+    def release_write(self):
+        self.lock.release()
+
+
 class Session:
     def __init__(self, token):
-        self._token = token
+        self._rwlock = RWLock()
         self._emits = []
         self._ctrl = None
+        self._token = token
 
     async def onopen(self, emit):
-        if not self._emits:
-            self._ctrl = await WsGdbController.anew(self._token)
-        self._emits.append(emit)
-        # todo send push
+        await self._rwlock.acquire_write()
+        try:
+            if not self._emits:
+                self._ctrl = await WsGdbController.anew(self._token)
+            self._emits.append(emit)
+            # todo send push
+        finally:
+            self._rwlock.release_write()
 
     async def onmessage(self, emit, data):
-        if data['type'] == 'pull':
-            method = data['method']
-            params = data.get('params', ())
-            tag = data.get('tag')
-            try:
-                assert method in WsGdbController.PULL, 'no method'
-                r = await getattr(self._ctrl, method)(*params)
-                if tag is not None:
-                    emit({'type': 'pull', 'tag': tag, 'r': suit_js(r), 'e': None, })
-            except BaseException as e:
-                if tag is not None:
-                    emit({'type': 'pull', 'tag': tag, 'r': None, 'e': suit_js(e), })
+        await self._rwlock.acquire_read()
+        try:
+            if data['type'] == 'pull':
+                method = data['method']
+                params = data.get('params', ())
+                tag = data.get('tag')
+                try:
+                    assert method in WsGdbController.PULL, 'no method'
+                    r = await getattr(self._ctrl, method)(*params)
+                    if tag is not None:
+                        emit({'type': 'pull', 'tag': tag, 'r': suit_js(r), 'e': None, })
+                except BaseException as e:
+                    if tag is not None:
+                        emit({'type': 'pull', 'tag': tag, 'r': None, 'e': suit_js(e), })
+        finally:
+            self._rwlock.release_read()
 
     async def onclose(self, emit):
-        self._emits.remove(emit)
-        if not self._emits:
-            await self._ctrl.adel()
-            self._ctrl = None
+        await self._rwlock.acquire_write()
+        try:
+            self._emits.remove(emit)
+            if not self._emits:
+                await self._ctrl.adel()
+                self._ctrl = None
+        finally:
+            self._rwlock.release_write()
 
 
 class WsGdbController(GdbController):
