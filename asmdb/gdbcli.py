c2d1063abbbd79310f17578b20a3fe7ac1aed01a
ZhangLiang <qimuyangtao@gmail.com>
2019-09-01 16:56:42 +0800

    use gdbcli

--- asmdb/gdbcli.py
+++ asmdb/gdbcli.py
@@ -2,26 +2,20 @@
 import signal
 import asyncio
 from asyncio import subprocess
-from pygdbmi.gdbmiparser import parse_response
 
 
 async def gdb_startup(config):
     stdin = stdout = subprocess.PIPE
     stderr = subprocess.DEVNULL
-    process = await asyncio.create_subprocess_exec('gdb', '--nx', '--interpreter=mi2', '--quiet', stdin=stdin, stdout=stdout, stderr=stderr)
+    process = await asyncio.create_subprocess_exec('gdb', '--nx', '--quiet', stdin=stdin, stdout=stdout, stderr=stderr)
     await gdb_readlines(process.stdout)
     return process
 
 
 async def gdb_readlines(stream):
-    lines = []
-    while True:
-        line = await stream.readline()
-        if not line:
-            return lines
-        lines.append(parse_response(line.decode()))
-        if lines[-1]['type'] == 'done':
-            return lines
+    lines = b''
+    # todo
+    return lines
 
 
 class GdbError(RuntimeError):
@@ -46,13 +40,7 @@ class GdbController:
     async def _command(self, command):
         async with self.cmdlock:
             self.process.stdin.write(command.encode() + b'\n')
-            for line in await gdb_readlines(self.process.stdout):
-                if line['type'] == 'result':
-                    if line['message'] == 'done':
-                        return line['payload']
-                    elif line['message'] == 'error':
-                        raise GdbError(line['payload']['msg'])
-            raise GdbError('no result')
+            return (await gdb_readlines(self.process.stdout)).decode()
 
     async def next(self):
         await self._command('nexti')
