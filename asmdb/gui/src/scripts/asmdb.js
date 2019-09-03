b02647563f3743dfc3d41d139d3bcc160c0786a6
ZhangLiang <qimuyangtao@gmail.com>
2019-09-03 12:51:44 +0800

    z

--- asmdb/gui/src/scripts/asmdb.js
+++ asmdb/gui/src/scripts/asmdb.js
@@ -6,50 +6,34 @@ function for_test(bytes) {
   return str;
 }
 
+var stack = null;
+var memory = null;
+
+setTimeout(() => {
+  if (stack) {
+    var bytes = [];
+    for (var i = 0; i < 10 * 256; i++) {
+      bytes[bytes.length] = Math.floor(256 * Math.random());
+    }
+    stack.onBreak(1, for_test(bytes));
+  }
+  if (memory) {
+    bytes = [];
+    for (var i = 0; i < 4 * 512; i++) {
+      bytes[bytes.length] = Math.floor(256 * Math.random());
+    }
+    memory.onBreak(2048, for_test(bytes));
+  }
+}, 1000);
+
 function registerEvent(type, object) {
   //todo
   switch (type) {
     case 'stack':
-      setTimeout(() => {
-        var bytes = [];
-        for (var i = 0; i < 10 * 256; i++) {
-          bytes[bytes.length] = Math.floor(256 * Math.random());
-        }
-        object.cache = bytes;
-        object.onBreak(1, for_test(bytes));
-      }, 1000);
-      setTimeout(() => {
-        var bytes = [];
-        for (var i = 0; i < 10 * 256; i++) {
-          if (Math.random() > 0.98) {
-            bytes[bytes.length] = Math.floor(256 * Math.random());
-          } else {
-            bytes[bytes.length] = object.cache[bytes.length];
-          }
-        }
-        object.onBreak(1, for_test(bytes));
-      }, 2000);
+      stack = object;
       break;
     case 'memory':
-      setTimeout(() => {
-        var bytes = [];
-        for (var i = 0; i < 4 * 512; i++) {
-          bytes[bytes.length] = Math.floor(256 * Math.random());
-        }
-        object.cache = bytes;
-        object.onBreak(2048, for_test(bytes));
-      }, 1000);
-      setTimeout(() => {
-        var bytes = [];
-        for (var i = 0; i < 4 * 512; i++) {
-          if (Math.random() > 0.98) {
-            bytes[bytes.length] = Math.floor(256 * Math.random());
-          } else {
-            bytes[bytes.length] = object.cache[bytes.length];
-          }
-        }
-        object.onBreak(2048, for_test(bytes));
-      }, 2000);
+      memory = object;
       break;
   }
 }
