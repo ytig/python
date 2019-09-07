9237e8eb7c7a87a60dff6320dc314c8eb677f4f5
ZhangLiang <qimuyangtao@gmail.com>
2019-09-07 17:50:15 +0800

    hst scroll

--- asmdb/gui/src/views/Recycler.vue
+++ asmdb/gui/src/views/Recycler.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="recycler-container" @scroll="onScroll">
-    <div v-for="item in items" :key="item.idx">
+  <div ref="container" class="recycler-container" @scroll="onScroll">
+    <div v-for="item in items" :key="item.idx" :idx="item.idx">
       <slot :item="item"></slot>
     </div>
   </div>
@@ -8,15 +8,55 @@
 
 <script>
 export default {
+  data: function() {
+    return {
+      hst: null
+    };
+  },
   props: {
     items: Array
   },
-  watch: {
-    items: function() {
-      console.log('items changed');
-    }
+  beforeUpdate: function() {
+    this.hstSet();
+  },
+  updated: function() {
+    this.hstGet();
   },
   methods: {
+    hstSet: function() {
+      var container = this.$refs.container;
+      var scrollTop = container.scrollTop;
+      for (var i = 0; i < container.children.length; i++) {
+        var child = container.children[i];
+        scrollTop -= child.scrollHeight;
+        if (scrollTop < 0) {
+          this.hst = [child.getAttribute('idx'), child.scrollHeight + scrollTop];
+          return;
+        }
+      }
+      this.hst = null;
+    },
+    hstGet: function() {
+      var container = this.$refs.container;
+      var scrollTop = 0;
+      if (!this.hst) {
+        for (var i = 0; i < container.children.length / 2; i++) {
+          var child = container.children[i];
+          scrollTop += child.scrollHeight;
+        }
+        container.scrollTop = scrollTop;
+      } else {
+        for (var i = 0; i < container.children.length; i++) {
+          var child = container.children[i];
+          if (child.getAttribute('idx') == this.hst[0]) {
+            scrollTop += this.hst[1];
+            container.scrollTop = scrollTop;
+            return;
+          }
+          scrollTop += child.scrollHeight;
+        }
+      }
+    },
     onScroll: function() {
       console.log('scroll');
     }
