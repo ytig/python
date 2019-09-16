b52bfea73f3b5ae5455412b5b7ba58d5901b1569
ZhangLiang <qimuyangtao@gmail.com>
2019-09-17 00:08:48 +0800

    Pager

---
+++ asmdb/gui/src/views/Pager.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="pager-container"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    canSub: Boolean,
+    canAdd: Boolean
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less">
+@import '~@/styles/theme';
+
+.pager-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+}
+</style>
--- asmdb/gui/src/windows/Stack.vue
+++ asmdb/gui/src/windows/Stack.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="stack-container" :style="{width:windowWidth+'px'}" @mousedown="requestFocus" @mouseup="onMouseUp">
+    <Pager :canSub="page>0" :canAdd="page<10-1" @input="onClickIndex(page+arguments[0])"></Pager>
     <Navigation :name="'Stack'" :focus="focus" :disable="disable"></Navigation>
     <div ref="stackLayout" class="stack-layout">
       <Empty v-if="items.length==0" :text="'[no data]'" style="padding-top:12px;"></Empty>
@@ -214,6 +215,7 @@ export default {
 @import '~@/styles/theme';
 
 .stack-container {
+  position: relative;
   display: flex;
   flex-direction: column;
   .stack-layout {
