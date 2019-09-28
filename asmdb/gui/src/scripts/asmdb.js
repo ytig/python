1a55837c25981ae9ef9d1bd7d76800a0d223af16
ZhangLiang <qimuyangtao@gmail.com>
2019-09-28 22:24:08 +0800

    z

--- asmdb/gui/src/scripts/asmdb.js
+++ asmdb/gui/src/scripts/asmdb.js
@@ -46,14 +46,6 @@ setTimeout(() => {
       stack.onBreak(1, for_test(stack.testBytes));
     }, 2500);
   }
-  if (memory) {
-    bytes = [];
-    var range = memory.getRange();
-    for (var i = 0; i < range[1] - range[0]; i++) {
-      bytes[bytes.length] = Math.floor(256 * Math.random());
-    }
-    memory.onBreak(range[0], for_test(bytes));
-  }
 }, 1000);
 
 function registerEvent(type, object) {
