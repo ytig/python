38db5f27292eaf7b1e7538d5959c62c348094bb3
ZhangLiang <qimuyangtao@gmail.com>
2019-11-03 15:11:24 +0800

    z

--- asmdb/gui/src/scripts/keyboard.js
+++ asmdb/gui/src/scripts/keyboard.js
@@ -2,41 +2,37 @@ var cur = -1;
 var list = [];
 
 function registerWindow(object) {
-  if (!(object in list)) {
-    list[list.length] = object;
+  if (object in list) {
+    return;
   }
+  list.push(object);
 }
 
 function unregisterWindow(object) {
-  for (var i = 0; i < list.length; i++) {
-    if (list[i] == object) {
-      if (i == cur) {
-        object.onFocusChanged(false);
-        cur = -1;
-      }
-      list.splice(i, 1);
-      break;
-    }
+  if (!(object in list)) {
+    return;
+  }
+  var i = list.indexOf(object);
+  list.splice(i, 1);
+  if (i == cur) {
+    cur = -1;
+    object.onFocusChanged(false);
   }
 }
 
 function requestFocus(object) {
-  var _cur = cur;
-  cur = -1;
-  for (var i = 0; i < list.length; i++) {
-    if (list[i] == object) {
-      cur = i;
-      break;
-    }
+  if (!(object in list)) {
+    return;
   }
-  if (_cur != cur) {
-    if (_cur >= 0) {
-      list[_cur].onFocusChanged(false);
-    }
-    if (cur >= 0) {
-      list[cur].onFocusChanged(true);
-    }
+  var _object = cur >= 0 ? list[cur] : null;
+  cur = list.indexOf(object);
+  if (_object == object) {
+    return;
   }
+  if (_object != null) {
+    _object.onFocusChanged(false);
+  }
+  object.onFocusChanged(true);
 }
 
 document.addEventListener('keydown', function (event) {
