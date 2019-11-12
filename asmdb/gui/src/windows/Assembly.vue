24c82e99260961b038ecf106dab46f4cdcda7a65
ZhangLiang <qimuyangtao@gmail.com>
2019-11-12 22:00:46 +0800

    z

--- asmdb/gui/src/windows/Assembly.vue
+++ asmdb/gui/src/windows/Assembly.vue
@@ -2,7 +2,7 @@
   <div class="assembly-container">
     <Navigation :name="'Assembly'"></Navigation>
     <Scroller ref="scroller" class="assembly-scroller" :source="source" #default="props">
-      <Bytes :startAddress="0" :lineNumber="props.item.lineNumber" :value="props.item.value" :group="8" :canvasContext="props.offset+';'+props.context" :lazyLayout="props.scrolling"></Bytes>
+      <Bytes :startAddress="0" :lineNumber="props.item.lineNumber" :value="props.item.value" :group="16" :canvasContext="props.offset+';'+props.context" :lazyLayout="props.scrolling"></Bytes>
     </Scroller>
   </div>
 </template>
@@ -10,17 +10,25 @@
 <script>
 export default {
   data: function() {
-    var source = {}; //for test
-    for (var i = -99; i < 100; i++) {
-      source[i] = {
-        height: 18,
-        lineNumber: '' + i,
-        value: {
-          newBytes: '123',
-          oldBytes: ''
-        }
-      };
-    }
+    var source = { invalidate: 0 }; //for test
+    setTimeout(() => {
+      for (var i = -99; i < 100; i++) {
+        source[i] = {
+          height: 18,
+          lineNumber:
+            (i >= 0 ? '+' : '-') +
+            '0x' +
+            Math.abs(i)
+              .toString(16)
+              .zfill(3),
+          value: {
+            newBytes: '0000000000000000',
+            oldBytes: ''
+          }
+        };
+      }
+      source.invalidate++;
+    }, 1000);
     return {
       source: source
     };
@@ -36,7 +44,7 @@ export default {
   display: flex;
   flex-direction: column;
   .assembly-scroller {
-    width: 147px;
+    width: 555px;
     height: 0px;
     flex-grow: 1;
   }
