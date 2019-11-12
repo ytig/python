4b1eb159a6282ee2c3dbc97f974aa86c5374f510
ZhangLiang <qimuyangtao@gmail.com>
2019-11-12 14:41:09 +0800

    Scroller

---
+++ asmdb/gui/src/views/Scroller.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="scroller-container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+@import '~@/styles/theme';
+
+.scroller-container {
+}
+</style>
