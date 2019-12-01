a123fc0169e76aea846218a16391b15b7b526fe9
ZhangLiang <qimuyangtao@gmail.com>
2019-12-01 23:52:21 +0800

    drag

--- asmdb/gui/src/windows/Python3.vue
+++ asmdb/gui/src/windows/Python3.vue
@@ -1,24 +1,54 @@
 <template>
   <div class="python3-container">
-    Welcome to using ASM Debugger!
-    <br />Python3 is developing.
-    <br />>>>
+    <Resize class="python3-resize" :direction="'row'" @drag2="onDrag2" @dragend2="onDragEnd2"></Resize>
+    <div class="python3-test" :style="{height:windowWidth+'px'}">
+      Welcome to using ASM Debugger!
+      <br />Python3 is developing.
+      <br />>>>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      minHeight: 64,
+      maxHeight: 200,
+      curHeight: 64,
+      addHeight: 0
+    };
+  },
+  computed: {
+    windowWidth: function() {
+      return Math.min(Math.max(this.curHeight + this.addHeight, this.minHeight), this.maxHeight);
+    }
+  },
+  methods: {
+    onDrag2: function(delta) {
+      this.addHeight = -delta;
+    },
+    onDragEnd2: function() {
+      this.curHeight = this.windowWidth;
+      this.addHeight = 0;
+    }
+  }
+};
 </script>
 
 <style lang="less">
 @import '~@/styles/theme';
 
 .python3-container {
-  height: 64px;
-  padding-top: 6px;
-  padding-left: 12px;
-  line-height: 18px;
-  color: @color-text;
-  font-size: 12px;
+  .python3-resize {
+    height: 4px;
+  }
+  .python3-test {
+    padding-top: 6px;
+    padding-left: 12px;
+    line-height: 18px;
+    color: @color-text;
+    font-size: 12px;
+  }
 }
 </style>
