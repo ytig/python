b58d745e535ffb2a5302114d196cbb70b40bcb8e
ZhangLiang <qimuyangtao@gmail.com>
2019-12-03 02:18:54 +0800

    py window

--- asmdb/gui/src/windows/Python3.vue
+++ asmdb/gui/src/windows/Python3.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="python3-container">
+  <div class="python3-container" @wheel.passive="requestFocus" @mousedown="requestFocus" @mouseup="onMouseUp">
     <Resize class="python3-resize" :direction="'row'" :lowest="windowHeight==maxHeight" :uppest="windowHeight==minHeight" @drag2="onDrag2(-arguments[0])" @dragend2="onDragEnd2"></Resize>
-    <NavigationPy></NavigationPy>
+    <NavigationPy :focus="focus" @mouseup2="onMouseUp2"></NavigationPy>
     <div class="python3-test" :style="{height:windowHeight+'px'}">
       Welcome to using ASM Debugger!
       <br />Python3 is developing.
@@ -11,13 +11,16 @@
 </template>
 
 <script>
+import keyboard from '@/scripts/keyboard';
+
 export default {
   data: function() {
     return {
       minHeight: 64,
       maxHeight: 200,
       curHeight: 0,
-      addHeight: 0
+      addHeight: 0,
+      focus: false
     };
   },
   computed: {
@@ -29,7 +32,30 @@ export default {
     var curHeight = loadStorage('python3_height', 0);
     this.curHeight = Math.min(Math.max(curHeight, this.minHeight), this.maxHeight);
   },
+  mounted: function() {
+    keyboard.registerWindow(this);
+  },
+  destroyed: function() {
+    keyboard.unregisterWindow(this);
+  },
   methods: {
+    requestFocus: function() {
+      keyboard.requestFocus(this);
+    },
+    onFocusChanged: function(value) {
+      this.focus = value;
+    },
+    onMouseUp: function(event) {
+      if (event.button == 2) {
+        this.$menu.close();
+      }
+    },
+    onMouseUp2: function(evnet) {
+      this.$menu.alert(event);
+    },
+    onKeyDown: function(event) {
+      return false;
+    },
     onDrag2: function(delta) {
       this.addHeight = delta;
     },
