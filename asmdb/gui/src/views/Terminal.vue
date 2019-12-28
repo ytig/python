1f175b29a9bf2e2cf852fd72f8b65c2cf034b68e
ZhangLiang <qimuyangtao@gmail.com>
2019-12-28 20:26:38 +0800

    z

--- asmdb/gui/src/views/Terminal.vue
+++ asmdb/gui/src/views/Terminal.vue
@@ -17,17 +17,56 @@ import TerminalChild from './Terminal_child';
 //input -> writeb 粘贴，中文输入：悬浮
 //scroll infinite
 class Source {
-  constructor(width, utf8) {
-    //todo
-    var i = 0;
-    for (var line of utf8.split('\r\n')) {
-      this[i++] = {
-        value: line,
-        styles: JSON.stringify([[line.length, '', '']]),
-        height: TerminalChild.measureHeight(width, line)
-      };
-    }
+  constructor() {
+    this.width = 0;
+    this.height = 0;
+    this.length = 0;
+    this.cursor = 0;
+    this.background = '';
+    this.color = '';
     this.invalidate = 0;
+    this.readu('\r\n');
+  }
+
+  setwinsize(width, height) {
+    if (this.width != width) {
+      this.width = width;
+      for (var i = 0; i < this.length; i++) {
+        this[i].height = TerminalChild.measureHeight(this.width, this[i].value);
+      }
+      this.invalidate++;
+    }
+    if (this.height != height) {
+      this.height = height;
+    }
+  }
+
+  readu(utf8) {
+    if (!utf8) {
+      return;
+    }
+    if (utf8.indexOf('\r\n') >= 0) {
+      var i = 0;
+      for (var line of utf8.split('\r\n')) {
+        if (i++ != 0) {
+          this[this.length++] = {
+            value: '',
+            styles: '[]',
+            height: TerminalChild.measureHeight(this.width, '')
+          };
+          this.cursor = 0;
+          this.invalidate++;
+        }
+        this.readu(line);
+      }
+      return;
+    }
+    var newValue = this[this.length - 1].value.substring(0, this.cursor) + utf8 + this[this.length - 1].value.substring(this.cursor);
+    this[this.length - 1].value = newValue;
+    this[this.length - 1].styles = JSON.stringify([[newValue.length, '', '']]); //todo
+    this[this.length - 1].height = TerminalChild.measureHeight(this.width, newValue);
+    this.cursor += utf8.length;
+    this.invalidate++;
   }
 }
 
@@ -53,7 +92,11 @@ export default {
           this.$nextTick(callee.bind(this, ...arguments));
           return;
         }
-        this.source = new Source(this.$el.clientWidth - 24, newValue);
+        if (this.source == null) {
+          this.source = new Source();
+          this.onResize();
+        }
+        this.source.readu(oldValue == undefined ? newValue : newValue.substring(oldValue.length));
       }
     }
   },
@@ -64,7 +107,11 @@ export default {
     resize.unregisterEvent(this);
   },
   methods: {
-    onResize: function() {}
+    onResize: function() {
+      if (this.source != null) {
+        this.source.setwinsize(this.$el.clientWidth - 24, this.$el.clientHeight);
+      }
+    }
   }
 };
 </script>
