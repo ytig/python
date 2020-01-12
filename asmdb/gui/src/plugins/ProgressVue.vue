23376fe20610787592588944c61ec139601262f3
ZhangLiang <qimuyangtao@gmail.com>
2020-01-12 22:52:25 +0800

    z

---
+++ asmdb/gui/src/plugins/ProgressVue.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-show="anim.value!=0" class="progress-container" :style="{background:background}">
+    <div class="progress-grow"></div>
+    <div class="progress-content" :style="{opacity:opacity}">{{message}}</div>
+    <div class="progress-grow"></div>
+  </div>
+</template>
+
+<script>
+import Animation from '@/scripts/animation';
+
+export default {
+  data: function() {
+    return {
+      message: '',
+      anim: new Animation(Animation.ease_out(224))
+    };
+  },
+  computed: {
+    background: function() {
+      return 'rgba(0, 0, 0, ' + this.anim.value * 0.5 + ')';
+    },
+    opacity: function() {
+      return this.anim.value;
+    }
+  },
+  methods: {
+    alert: function(message) {
+      this.anim.$target(1);
+      this.message = message;
+    },
+    close: function() {
+      this.anim.$target(0);
+    },
+    onKeyDown: function(event) {
+      var show = this.anim.value != this.anim.target || this.anim.value != 0;
+      return show;
+    }
+  }
+};
+</script>
+
+<style lang="less">
+@import '~@/styles/theme';
+
+.progress-container {
+  position: fixed;
+  z-index: 7;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .progress-grow {
+    flex-grow: 1;
+  }
+  .progress-content {
+    font-size: 12px;
+    color: @color-text-menu;
+  }
+}
+</style>
