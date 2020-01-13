67760e7e4b3eec4f1b2e71872aa17c56f99f62f6
ZhangLiang <qimuyangtao@gmail.com>
2020-01-14 02:34:06 +0800

    initTween

--- asmdb/gui/src/Hello.vue
+++ asmdb/gui/src/Hello.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="hello-container" @click="startDebug">
+    <canvas id="tween"></canvas>
     <span>Hello World!</span>
   </div>
 </template>
 
 <script>
+var tweenAlive = false;
+
+function initTween() {
+  tweenAlive = true;
+  var canvas = document.getElementById('tween');
+  var s = devicePixelRatio;
+  canvas.style.width = screen.width + 'px';
+  canvas.style.height = screen.height + 'px';
+  canvas.width = devicePixelRatio * screen.width;
+  canvas.height = devicePixelRatio * screen.height;
+  requestAnimationFrames(() => {
+    if (!initCanvas) {
+      return false;
+    }
+    if (tweenAlive) {
+      initCanvas('tween');
+    }
+    return true;
+  });
+}
+
+function killTween() {
+  kill();
+  TweenLite.ticker.sleep();
+  C = c = undefined;
+  tweenAlive = false;
+}
+
 export default {
+  mounted: function() {
+    initTween();
+  },
+  destroyed: function() {
+    killTween();
+  },
   methods: {
     startDebug: function() {
       document.body.webkitRequestFullScreen();
@@ -19,11 +54,14 @@ export default {
 @import '~@/styles/theme';
 
 .hello-container {
+  position: relative;
   background: @color-background;
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
+  overflow: hidden;
+  > canvas {
+    position: absolute;
+  }
   > span {
     align-self: center;
     font-size: 16px;
