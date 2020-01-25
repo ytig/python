2cb58f61bcc6148e6546de55e322b43e30a6c575
ZhangLiang <qimuyangtao@gmail.com>
2020-01-25 12:21:04 +0800

    z

--- asmdb/gui/src/Hello.vue
+++ asmdb/gui/src/Hello.vue
@@ -103,7 +103,7 @@ export default {
     updateDeviceAssist: function() {
       this.deviceAssist = null; //todo
       setTimeout(() => {
-        this.deviceAssist = ['android://123', 'mac://0', 'mac://1', 'mac://2', 'mac://3', 'mac://4', 'mac://5'];
+        this.deviceAssist = ['android://123', 'mac://0', 'mac://1', 'mac://2', 'mac://3', 'mac://4', 'mac://5', 'mac://6', 'mac://7', 'mac://8', 'mac://9'];
       }, 3000);
     },
     updateProcessAssist: function() {
