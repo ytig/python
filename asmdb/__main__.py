a2b8540b13bb9c2b3043159dd764186a8c010e57
ZhangLiang <qimuyangtao@gmail.com>
2020-01-26 13:31:54 +0800

    assist server

--- asmdb/__main__.py
+++ asmdb/__main__.py
@@ -14,6 +14,51 @@ async def index(request):
 app.router.add_get('/', index)
 
 
+async def assist_device():
+    r = []
+    return r
+
+
+async def assist_process(device):
+    r = []
+    return r
+
+
+async def assist_script(script):
+    r = []
+    try:
+        driname = os.path.dirname(script) or '/'
+        for filename in os.listdir(driname):
+            if filename.startswith('.'):
+                continue
+            filepath = os.path.join(driname, filename)
+            if os.path.isdir(filepath):
+                filepath += '/'
+            r.append(filepath)
+    except OSError:
+        pass
+    return r
+
+
+def assist_filter(a, v):
+    return filter(lambda i: i.startswith(v) and i != v, a)
+
+
+async def assist(request):
+    _assist = []
+    _type = request.query['type']
+    _value = request.query['value']
+    if _type == 'device':
+        _assist.extend(assist_filter(await assist_device(), _value))
+    elif _type == 'process':
+        _device, = json.loads(request.query['context'])
+        _assist.extend(assist_filter(await assist_process(_device), _value))
+    elif _type == 'script':
+        _assist.extend(assist_filter(await assist_script(_value), _value))
+    return web.json_response(_assist)
+app.router.add_get('/assist', assist)
+
+
 async def websocket(request):
     token = request.cookies.get('token')
     response = web.WebSocketResponse()
