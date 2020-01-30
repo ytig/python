586fb1a70f2056963e510048b5d858bd2d2bb78a
ZhangLiang <qimuyangtao@gmail.com>
2020-01-31 03:47:00 +0800

    wsController

--- asmdb/__init__.py
+++ asmdb/__init__.py
@@ -1,1 +1,17 @@
 #!/usr/local/bin/python3
+import sys
+
+
+class WsController:
+    def __init__(self, host, port, token):
+        # todo
+        pass
+
+
+_ctrl = None
+
+
+def ctrl():
+    if _ctrl is None:
+        _ctrl = WsController('0.0.0.0', 8519, sys.argv[1])
+    return _ctrl
