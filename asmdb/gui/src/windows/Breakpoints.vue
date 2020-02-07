8327ac6cd00d02c1cc34e549313770a349983afb
ZhangLiang <qimuyangtao@gmail.com>
2020-02-08 01:40:00 +0800

    bpt icon unclickable

--- asmdb/gui/src/windows/Breakpoints.vue
+++ asmdb/gui/src/windows/Breakpoints.vue
@@ -182,8 +182,8 @@ export default {
       display: flex;
       align-items: center;
       > span:nth-of-type(1) {
-        width: 8px;
-        height: 8px;
+        width: 5px;
+        height: 5px;
         border-radius: 999px;
         background: @color-icon-breakpoint;
         margin-right: 8px;
@@ -217,6 +217,7 @@ export default {
       }
       > span:nth-of-type(2) {
         color: @color-text-dark;
+        text-decoration: line-through;
       }
     }
     .breakpoints-func {
